swgemu/Core3

[added] Using the looted datapad from the Sith intro theater. [added] Go to location generic quest module. [added] Go to Dathomir quest extension to force sensitive intro, with Lua tests for extension and Sith intro camp using disc. [added] Quests enum in player variables. [added] Players with the quest active or completed needed to get into the Village active area now can. [added] Rohak to village spawner.
Change-Id: I67b7a00205ec2a008059af10346e79f22056dc84

MMOCoreORB/bin/scripts/managers/jedi/village/sith_shadow_intro_theater.lua
@@ -3,6 +3,9 @@ local ObjectManager = require("managers.object.object_manager")
 local QuestManager = require("managers.quest.quest_manager")
 local SpawnMobiles = require("utils.spawn_mobiles")
 require("utils.helpers")
+local DathomirGoTo = require("managers.jedi.village.dathomir_go_to")
+local READ_DISK_2_STRING = "@quest/force_sensitive/intro:read_disk2"
+local READ_DISK_ERROR_STRING = "@quest/force_sensitive/intro:read_disk_error"
 
 SithShadowIntroTheater = GoToTheater:new {
 	-- Task properties
@@ -108,6 +111,28 @@ function SithShadowIntroTheater:onPlayerKilled(pCreatureObject, pKiller, nothing
 	end
 
 	return 0
+end
+
+-- Handling of the activation of the theater waypoint datapad.
+-- @param pSceneObject pointer to the datapad object.
+-- @param pCreatureObject pointer to the creature object who activated the datapad.
+function SithShadowIntroTheater:useTheaterDatapad(pSceneObject, pCreatureObject)
+	Logger:log("Player used the looted theater datapad.", LT_INFO)
+	if QuestManager.hasCompletedQuest(pCreatureObject, QuestManager.quests.GOT_DATAPAD_2) then
+		DathomirGoTo:start(pCreatureObject)
+		ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+			creatureObject:sendSystemMessage(READ_DISK_2_STRING)
+		end)
+		ObjectManager.withSceneObject(pSceneObject, function(sceneObject)
+			sceneObject:destroyObjectFromWorld()
+		end)
+		QuestManager.completeQuest(pCreatureObject, QuestManager.quests.LOOT_DATAPAD_2)
+		QuestManager.activateQuest(pCreatureObject, QuestManager.quests.FS_VILLAGE_ELDER)
+	else
+		ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+			creatureObject:sendSystemMessage(READ_DISK_ERROR_STRING)
+		end)
+	end
 end
 
 return SithShadowIntroTheater

MMOCoreORB/bin/scripts/quest/tasks/go_to_location.lua
@@ -1,0 +1,104 @@
+-- This module handles the go_to_location script for the quests.
+-- It spawns an active area in the world where the player must go.
+-- Parameters:
+--  - waypointDescription 	- description for the waypoint that is shown for the player.
+
+local Task = require("quest.tasks.task")
+local ObjectManager = require("managers.object.object_manager")
+local Logger = require("utils.logger")
+
+local ACTIVE_AREA_IFF = "object/active_area.iff"
+
+local ACTIVE_AREA_ID_STRING = "activeAreaId"
+local WAYPOINT_ID_STRING = "waypointId"
+
+GoToLocation = Task:new {
+	-- Task properties
+	taskName = "",
+	-- GoToLocation properties
+	waypointDescription = "",
+	spawnPoint = {},
+	spawnPlanet = "",
+	spawnRadius = nil
+}
+
+
+-- Setup the active area around.
+-- @param pCreatureObject pointer to the creature object for whom the area is created for.
+-- @param spawnPointCoordinates where to spawn the active area.
+-- @param spawnPointPlanet planet to spawn it on.
+-- @return true if setup was successful, false otherwise.
+function GoToLocation:setupActiveArea(pCreatureObject, spawnPoint, spawnPlanet, spawnRadius)
+	return ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+		local pActiveArea = spawnSceneObject(spawnPlanet, ACTIVE_AREA_IFF, spawnPoint.x, 0, spawnPoint.y, 0, 0)
+
+		return ObjectManager.withActiveArea(pActiveArea, function(activeArea)
+			return ObjectManager.withSceneObject(pActiveArea, function(activeAreaSceneObject)
+				writeData(creatureObject:getObjectID() .. self.taskName .. ACTIVE_AREA_ID_STRING, activeAreaSceneObject:getObjectID())
+				activeArea:setRadius(spawnRadius)
+				createObserver(ENTEREDAREA, self.taskName, "handleEnteredAreaEvent", pActiveArea)
+				return true
+			end) == true
+		end) == true
+	end) == true
+end
+
+-- Handle the entered active area event.
+-- @param pActiveArea pointer to the active area that is entered.
+-- @param pCreatureObject pointer to the creature who is entering the active area.
+-- @param nothing not used.
+function GoToLocation:handleEnteredAreaEvent(pActiveArea, pCreatureObject, nothing)
+	ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+		local storedActiveAreaId = readData(creatureObject:getObjectID() .. self.taskName .. ACTIVE_AREA_ID_STRING)
+		ObjectManager.withSceneObject(pActiveArea, function(activeArea)
+			if storedActiveAreaId == activeArea:getObjectID() then
+				self:onEnteredActiveArea(pCreatureObject)
+			end
+		end)
+	end)
+end
+
+-- Start the GoToLocation.
+-- @param pCreatureObject pointer to the creature object of the player.
+function GoToLocation:taskStart(pCreatureObject)
+	Logger:log("Spawning " .. self.taskName .. " active area location.", LT_INFO)
+	ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+		if self:setupActiveArea(pCreatureObject, self.spawnPoint, self.spawnPlanet, self.spawnRadius) then
+			local waypointId = ObjectManager.withPlayerObject(creatureObject:getPlayerObject(), function(playerObject)
+				return playerObject:addWaypoint(self.spawnPlanet, self.waypointDescription, "", self.spawnPoint.x, self.spawnPoint.y, WAYPOINTORANGE, true, true, 0)
+			end)
+
+			if waypointId ~= nil then
+				writeData(creatureObject:getObjectID() .. self.taskName .. WAYPOINT_ID_STRING, waypointId)
+				return true
+			end
+		end
+
+		-- Something failed above, clean up and end the task.
+		Logger:log("Failed to spawn " .. self.taskName .. " location.", LT_ERROR)
+		self:finish(pCreatureObject)
+	end)
+
+	return false
+end
+
+-- Handle the task finish event.
+-- @param pCreatureObject pointer to the creature object of the player that the event was triggered for.
+function GoToLocation:taskFinish(pCreatureObject)
+	Logger:log("Despawning " .. self.taskName .. " location.", LT_INFO)
+	ObjectManager.withCreatureAndPlayerObject(pCreatureObject, function(creatureObject, playerObject)
+		local waypointId = readData(creatureObject:getObjectID() .. self.taskName .. WAYPOINT_ID_STRING)
+		playerObject:removeWaypoint(waypointId, true)
+
+		local activeAreaId = readData(creatureObject:getObjectID() .. self.taskName .. ACTIVE_AREA_ID_STRING)
+		ObjectManager.withSceneObjectFromId(activeAreaId, function(activeArea)
+			activeArea:destroyObjectFromWorld()
+		end)
+
+		self:finish(pCreatureObject)
+	end)
+
+	return true
+end
+
+return GoToLocation

TheTinyPebble/swgtcw-test

MMOCoreORB/bin/scripts/managers/jedi/village/sith_shadow_intro_theater.lua
@@ -3,6 +3,9 @@ local ObjectManager = require("managers.object.object_manager")
 local QuestManager = require("managers.quest.quest_manager")
 local SpawnMobiles = require("utils.spawn_mobiles")
 require("utils.helpers")
+local GoToDathomir = require("managers.jedi.village.go_to_dathomir")
+local READ_DISK_2_STRING = "@quest/force_sensitive/intro:read_disk2"
+local READ_DISK_ERROR_STRING = "@quest/force_sensitive/intro:read_disk_error"
 
 SithShadowIntroTheater = GoToTheater:new {
 	-- Task properties
@@ -108,6 +111,27 @@ function SithShadowIntroTheater:onPlayerKilled(pCreatureObject, pKiller, nothing
 	end
 
 	return 0
+end
+
+-- Handling of the activation of the theater waypoint datapad.
+-- @param pSceneObject pointer to the datapad object.
+-- @param pCreatureObject pointer to the creature object who activated the datapad.
+function SithShadowIntroTheater:useTheaterDatapad(pSceneObject, pCreatureObject)
+	Logger:log("Player used the looted theater datapad.", LT_INFO)
+	if QuestManager.hasCompletedQuest(pCreatureObject, QuestManager.quests.GOT_DATAPAD_2) then
+		ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+			creatureObject:sendSystemMessage(READ_DISK_2_STRING)
+		end)
+		ObjectManager.withSceneObject(pSceneObject, function(sceneObject)
+			sceneObject:destroyObjectFromWorld()
+		end)
+		QuestManager.completeQuest(pCreatureObject, QuestManager.quests.LOOT_DATAPAD_2)
+        GoToDathomir:start(pCreatureObject)
+	else
+		ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+			creatureObject:sendSystemMessage(READ_DISK_ERROR_STRING)
+		end)
+	end
 end
 
 return SithShadowIntroTheater

MMOCoreORB/bin/scripts/quest/tasks/go_to_location.lua
@@ -1,0 +1,109 @@
+-- This module handles the go_to_location script for the quests.
+-- It spawns an active area in the world where the player must go.
+-- Parameters:
+--  - waypointDescription 	- description for the waypoint that is shown for the player.
+
+local Task = require("quest.tasks.task")
+local ObjectManager = require("managers.object.object_manager")
+local Logger = require("utils.logger")
+
+local ACTIVE_AREA_IFF = "object/active_area.iff"
+
+local ACTIVE_AREA_ID_STRING = "activeAreaId"
+local WAYPOINT_ID_STRING = "waypointId"
+
+GoToLocation = Task:new {
+	-- Task properties
+	taskName = "",
+	-- GoToLocation properties
+	waypointDescription = "",
+	spawnPoint = {},
+	spawnPlanet = "",
+	spawnRadius = nil,
+    onFailedSpawn = nil,
+	onSuccessfulSpawn = nil,
+	onEnteredActiveArea = nil
+}
+
+
+-- Setup the active area around.
+-- @param pCreatureObject pointer to the creature object for whom the area is created for.
+-- @param spawnPointCoordinates where to spawn the active area.
+-- @param spawnPointPlanet planet to spawn it on.
+-- @return true if setup was successful, false otherwise.
+function GoToLocation:setupActiveArea(pCreatureObject, spawnPoint, spawnPlanet, spawnRadius)
+	return ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+		local pActiveArea = spawnSceneObject(spawnPlanet, ACTIVE_AREA_IFF, spawnPoint.x, 0, spawnPoint.y, 0, 0)
+
+		return ObjectManager.withActiveArea(pActiveArea, function(activeArea)
+			return ObjectManager.withSceneObject(pActiveArea, function(activeAreaSceneObject)
+				writeData(creatureObject:getObjectID() .. self.taskName .. ACTIVE_AREA_ID_STRING, activeAreaSceneObject:getObjectID())
+				activeArea:setRadius(spawnRadius)
+				createObserver(ENTEREDAREA, self.taskName, "handleEnteredAreaEvent", pActiveArea)
+				return pActiveArea
+			end)
+		end)
+	end)
+end
+
+-- Handle the entered active area event.
+-- @param pActiveArea pointer to the active area that is entered.
+-- @param pCreatureObject pointer to the creature who is entering the active area.
+-- @param nothing not used.
+function GoToLocation:handleEnteredAreaEvent(pActiveArea, pCreatureObject, nothing)
+	ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+		local storedActiveAreaId = readData(creatureObject:getObjectID() .. self.taskName .. ACTIVE_AREA_ID_STRING)
+		ObjectManager.withSceneObject(pActiveArea, function(activeArea)
+			if storedActiveAreaId == activeArea:getObjectID() then
+				self:onEnteredActiveArea(pCreatureObject)
+			end
+		end)
+	end)
+end
+
+-- Start the GoToLocation.
+-- @param pCreatureObject pointer to the creature object of the player.
+function GoToLocation:taskStart(pCreatureObject)
+	Logger:log("Spawning " .. self.taskName .. " active area location.", LT_INFO)
+	ObjectManager.withCreatureObject(pCreatureObject, function(creatureObject)
+		local pActiveArea = self:setupActiveArea(pCreatureObject, self.spawnPoint, self.spawnPlanet, self.spawnRadius)
+		if pActiveArea ~= nil then
+			local waypointId = ObjectManager.withPlayerObject(creatureObject:getPlayerObject(), function(playerObject)
+				return playerObject:addWaypoint(self.spawnPlanet, self.waypointDescription, "", self.spawnPoint.x, self.spawnPoint.y, WAYPOINTORANGE, true, true, 0)
+			end)
+
+			if waypointId ~= nil then
+				writeData(creatureObject:getObjectID() .. self.taskName .. WAYPOINT_ID_STRING, waypointId)
+				self:onSuccessfulSpawn(pCreatureObject, pActiveArea)
+				return true
+			end
+		end
+
+		-- Something failed above, clean up and end the task.
+		Logger:log("Failed to spawn " .. self.taskName .. " location.", LT_ERROR)
+		self:finish(pCreatureObject)
+	end)
+
+	return false
+end
+
+-- Handle the task finish event.
+-- @param pCreatureObject pointer to the creature object of the player that the event was triggered for.
+function GoToLocation:taskFinish(pCreatureObject)
+	Logger:log("Despawning " .. self.taskName .. " location.", LT_INFO)
+	ObjectManager.withCreatureAndPlayerObject(pCreatureObject, function(creatureObject, playerObject)
+		local waypointId = readData(creatureObject:getObjectID() .. self.taskName .. WAYPOINT_ID_STRING)
+		playerObject:removeWaypoint(waypointId, true)
+
+		local activeAreaId = readData(creatureObject:getObjectID() .. self.taskName .. ACTIVE_AREA_ID_STRING)
+		ObjectManager.withSceneObjectFromId(activeAreaId, function(activeArea)
+			activeArea:destroyObjectFromWorld()
+		end)
+
+		self:finish(pCreatureObject)
+	end)
+
+	return true
+end
+
+return GoToLocation