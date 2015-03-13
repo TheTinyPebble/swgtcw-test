swgemu/Core3

[added] hitbox-style radii so mobs don't sit right on top of you
Change-Id: I79c6a589240b8fa938b6a43746b7cbe6278b7666

MMOCoreORB/src/server/zone/objects/creature/commands/CombatQueueCommand.h
@@ -131,10 +131,10 @@ public:
 		if (creature->isKneeling() && weapon->isMeleeWeapon() && range == -1 && !weapon->isJediWeapon())
 			return NOKNEELING;
 
-		if (creature->isProne() && (!weapon->isRangedWeapon() || targetObject->isInRange(creature, 5)) && range == -1)
+		if (creature->isProne() && (!weapon->isRangedWeapon() || targetObject->isInRange(creature, 5 + targetObject->getRadius() + creature->getRadius())) && range == -1)
 			return NOPRONE;
 
-		if (!targetObject->isInRange(creature, checkRange))
+		if (!targetObject->isInRange(creature, checkRange + targetObject->getRadius() + creature->getRadius()))
 			return TOOFAR;
 
 		if (!CollisionManager::checkLineOfSight(creature, targetObject)) {

MMOCoreORB/src/server/zone/objects/creature/commands/DiagnoseCommand.h
@@ -82,7 +82,7 @@ public:
 
 		Locker clocker(creatureTarget, creature);
 
-		if (!creatureTarget->isInRange(creature, range))
+		if (!creatureTarget->isInRange(creature, range + creatureTarget->getRadius() + creature->getRadius()))
 			return TOOFAR;
 			
 		if (!creatureTarget->isHealableBy(creature)) {

MMOCoreORB/src/server/zone/objects/creature/commands/DotPackCommand.h
@@ -270,7 +270,7 @@ public:
 
 		int	range = int(dotPack->getRange() + creature->getSkillMod("healing_range") / 100 * 14);
 
-		if (creature != creatureTarget && !creature->isInRange(creatureTarget, range)){
+		if (creature != creatureTarget && !creature->isInRange(creatureTarget, range + creatureTarget->getRadius() + creature->getRadius())){
 			creature->sendSystemMessage("@error_message:target_out_of_range"); //Your target is out of range for this action.
 			return TOOFAR;
 		}

MMOCoreORB/src/server/zone/objects/creature/commands/DrainForceCommand.h
@@ -80,7 +80,7 @@ public:
 		if (targetCreature == NULL || targetCreature->isDead() || targetCreature->isIncapacitated() || !targetCreature->isAttackableBy(creature))
 			return INVALIDTARGET;
 
-		if (!targetCreature->isInRange(creature, range))
+		if (!targetCreature->isInRange(creature, range + targetCreature->getRadius() + creature->getRadius()))
 			return TOOFAR;
 
 		if (!CollisionManager::checkLineOfSight(creature, targetCreature)) {

MMOCoreORB/src/server/zone/objects/creature/commands/HealDamageCommand.h
@@ -432,7 +432,7 @@ public:
 		if (stimPack->isRangedStimPack())
 			rangeToCheck = (cast<RangedStimPack*>(stimPack.get()))->getRange();
 
-		if (!creature->isInRange(targetCreature, rangeToCheck))
+		if (!creature->isInRange(targetCreature, rangeToCheck + targetCreature->getRadius() + creature->getRadius()))
 			return TOOFAR;
 
 		if (creature != targetCreature && !CollisionManager::checkLineOfSight(creature, targetCreature)) {

MMOCoreORB/src/server/zone/objects/creature/commands/HealDroidWoundCommand.h
@@ -234,7 +234,7 @@ public:
 			return GENERALERROR;
 		}
 
-		if (!creature->isInRange(droid, range))
+		if (!creature->isInRange(droid, range + droid->getRadius() + creature->getRadius()))
 			return TOOFAR;
 
 		uint8 attribute = findAttribute(droid);

MMOCoreORB/src/server/zone/objects/creature/commands/TameCommand.h
@@ -76,7 +76,7 @@ public:
 
 		Creature* baby = cast<Creature*>(object.get());
 
-		if (!object->isInRange(creature, 8.0f)){
+		if (!object->isInRange(creature, 8.0f + object->getRadius() + creature->getRadius())){
 			creature->sendSystemMessage("@system_msg:out_of_range"); // You are out of range
 			return TOOFAR;
 		}

TheTinyPebble/swgtcw-test

MMOCoreORB/src/server/zone/objects/creature/commands/CombatQueueCommand.h
@@ -131,10 +131,10 @@ public:
 		if (creature->isKneeling() && weapon->isMeleeWeapon() && range == -1 && !weapon->isJediWeapon())
 			return NOKNEELING;
 
-		if (creature->isProne() && (!weapon->isRangedWeapon() || targetObject->isInRange(creature, 5)) && range == -1)
+		if (creature->isProne() && (!weapon->isRangedWeapon() || targetObject->isInRange(creature, 5 + targetObject->getTemplateRadius() + creature->getTemplateRadius())) && range == -1)
 			return NOPRONE;
 
-		if (!targetObject->isInRange(creature, checkRange))
+		if (!targetObject->isInRange(creature, checkRange + targetObject->getTemplateRadius() + creature->getTemplateRadius()))
 			return TOOFAR;
 
 		if (!CollisionManager::checkLineOfSight(creature, targetObject)) {

MMOCoreORB/src/server/zone/objects/creature/commands/DiagnoseCommand.h
@@ -82,7 +82,7 @@ public:
 
 		Locker clocker(creatureTarget, creature);
 
-		if (!creatureTarget->isInRange(creature, range))
+		if (!creatureTarget->isInRange(creature, range + creatureTarget->getTemplateRadius() + creature->getTemplateRadius()))
 			return TOOFAR;
 			
 		if (!creatureTarget->isHealableBy(creature)) {

MMOCoreORB/src/server/zone/objects/creature/commands/DotPackCommand.h
@@ -270,7 +270,7 @@ public:
 
 		int	range = int(dotPack->getRange() + creature->getSkillMod("healing_range") / 100 * 14);
 
-		if (creature != creatureTarget && !creature->isInRange(creatureTarget, range)){
+		if (creature != creatureTarget && !creature->isInRange(creatureTarget, range + creatureTarget->getTemplateRadius() + creature->getTemplateRadius())){
 			creature->sendSystemMessage("@error_message:target_out_of_range"); //Your target is out of range for this action.
 			return TOOFAR;
 		}

MMOCoreORB/src/server/zone/objects/creature/commands/DrainForceCommand.h
@@ -80,7 +80,7 @@ public:
 		if (targetCreature == NULL || targetCreature->isDead() || targetCreature->isIncapacitated() || !targetCreature->isAttackableBy(creature))
 			return INVALIDTARGET;
 
-		if (!targetCreature->isInRange(creature, range))
+		if (!targetCreature->isInRange(creature, range + targetCreature->getTemplateRadius() + creature->getTemplateRadius()))
 			return TOOFAR;
 
 		if (!CollisionManager::checkLineOfSight(creature, targetCreature)) {

MMOCoreORB/src/server/zone/objects/creature/commands/HealDamageCommand.h
@@ -432,7 +432,7 @@ public:
 		if (stimPack->isRangedStimPack())
 			rangeToCheck = (cast<RangedStimPack*>(stimPack.get()))->getRange();
 
-		if (!creature->isInRange(targetCreature, rangeToCheck))
+		if (!creature->isInRange(targetCreature, rangeToCheck + targetCreature->getTemplateRadius() + creature->getTemplateRadius()))
 			return TOOFAR;
 
 		if (creature != targetCreature && !CollisionManager::checkLineOfSight(creature, targetCreature)) {

MMOCoreORB/src/server/zone/objects/creature/commands/HealDroidWoundCommand.h
@@ -234,7 +234,7 @@ public:
 			return GENERALERROR;
 		}
 
-		if (!creature->isInRange(droid, range))
+		if (!creature->isInRange(droid, range + droid->getTemplateRadius() + creature->getTemplateRadius()))
 			return TOOFAR;
 
 		uint8 attribute = findAttribute(droid);

MMOCoreORB/src/server/zone/objects/creature/commands/TameCommand.h
@@ -76,7 +76,7 @@ public:
 
 		Creature* baby = cast<Creature*>(object.get());
 
-		if (!object->isInRange(creature, 8.0f)){
+		if (!object->isInRange(creature, 8.0f + object->getTemplateRadius() + creature->getTemplateRadius())){
 			creature->sendSystemMessage("@system_msg:out_of_range"); // You are out of range
 			return TOOFAR;
 		}