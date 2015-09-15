swgemu/Core3

[added] Aurilian Plant functionality
Change-Id: I8cf20456a690c2f2c632fcaf4b53755c171c28a8

MMOCoreORB/src/server/zone/objects/tangible/misc/PlantObjectImplementation.cpp
@@ -1,0 +1,279 @@
+#include "server/zone/objects/tangible/misc/PlantObject.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/packets/scene/AttributeListMessage.h"
+#include "server/zone/packets/object/ObjectMenuResponse.h"
+#include "server/zone/objects/player/sui/listbox/SuiListBox.h"
+#include "server/zone/objects/player/sui/callbacks/GrowablePlantSuiCallback.h"
+#include "server/zone/managers/crafting/CraftingManager.h"
+#include "server/zone/objects/resource/ResourceContainer.h"
+#include "server/zone/objects/building/BuildingObject.h"
+#include "server/zone/objects/tangible/tasks/GrowablePlantPulseTask.h"
+#include "server/zone/Zone.h"
+#include "server/zone/ZoneServer.h"
+
+void PlantObjectImplementation::initializeTransientMembers() {
+	initializePlant(1);
+
+	TangibleObjectImplementation::initializeTransientMembers();
+
+}
+
+void PlantObjectImplementation::fillObjectMenuResponse(ObjectMenuResponse* menuResponse, CreatureObject* player) {
+	TangibleObjectImplementation::fillObjectMenuResponse(menuResponse, player);
+
+	if (plantSize == 0)
+		return;
+
+	menuResponse->addRadialMenuItem(68, 3, "@plant_grow:nutrients");
+	menuResponse->addRadialMenuItemToRadialID(68, 69, 3, "@plant_grow:add_nutrients");
+	menuResponse->addRadialMenuItemToRadialID(68, 70, 3, "@plant_grow:remove_nutrients_menu");
+
+	menuResponse->addRadialMenuItem(71, 3, "@plant_grow:water");
+	menuResponse->addRadialMenuItemToRadialID(71, 72, 3, "@plant_grow:add_water");
+	menuResponse->addRadialMenuItemToRadialID(71, 73, 3, "@plant_grow:remove_water_menu");
+
+	if (fruitCount > 0)
+		menuResponse->addRadialMenuItem(74, 3, "@plant_grow:pick_fruit_menu");
+}
+
+int PlantObjectImplementation::handleObjectMenuSelect(CreatureObject* player, byte selectedID) {
+	ManagedReference<SceneObject*> parent = getRootParent();
+
+	if (parent == NULL || !parent->isBuildingObject()) {
+		player->sendSystemMessage("@plant_grow:must_be_in_building"); // The plant must be in a building which you administrate.
+		return 0;
+	}
+
+	ManagedReference<BuildingObject*> building = cast<BuildingObject*>( parent.get());
+
+	if ((isASubChildOf(player) || !building->isOnAdminList(player)) && selectedID >= 69 && selectedID <= 74) {
+		player->sendSystemMessage("@plant_grow:must_be_in_building"); // The plant must be in a building which you administrate.
+		return 0;
+	}
+
+	if (selectedID == 69) { // Add Nutrients
+		sendResourceSUI(player, 1);
+	} else if (selectedID == 72) { // Add Water
+		sendResourceSUI(player, 2);
+	} else if (selectedID == 70) { // Remove Nutrients
+		nutrientLevel -= 5;
+
+		if (nutrientLevel < 0)
+			nutrientLevel = 0;
+
+		player->sendSystemMessage("@plant_grow:remove_nutrients");
+	} else if (selectedID == 73) { // Remove Water
+		waterLevel -= 5;
+
+		if (waterLevel < 0)
+			waterLevel = 0;
+
+		player->sendSystemMessage("@plant_grow:remove_water");
+	} else if (selectedID == 74) { // Pick Fruit
+		if (fruitCount < 1)
+			return 0;
+
+		ManagedReference<SceneObject*> inventory = player->getSlottedObject("inventory");
+
+		if(inventory->isContainerFullRecursive()){
+			player->sendSystemMessage("@plant_grow:no_inventory"); // You do not have any inventory space.
+			return 0;
+		}
+
+		ZoneServer* zserv = player->getZoneServer();
+
+		fruitCount--;
+		player->sendSystemMessage("@plant_grow:pick_fruit"); // You pick a piece of fruit from the plant.
+
+		String fruitTemplate = "object/tangible/item/plant/force_melon.iff";
+		Reference<SceneObject*> fruit = zserv->createObject(fruitTemplate.hashCode(), 1);
+
+		if(fruit == NULL) {
+			return 0;
+		}
+
+		if (!inventory->transferObject(fruit, -1, false, false) ) {
+			fruit->destroyObjectFromDatabase(true);
+			return 0;
+		}
+
+		inventory->broadcastObject(fruit, true);
+	}
+
+	return 1;
+}
+
+void PlantObjectImplementation::fillAttributeList(AttributeListMessage* alm, CreatureObject* object) {
+	int temp = health;
+
+	if (temp > 0 && temp < 10)
+		temp = 10;
+	alm->insertAttribute("plant_health_n", "@plant_grow:health_" + String::valueOf(temp / 10));
+
+	if (fruitCount > 0)
+		alm->insertAttribute("plant_fruit", fruitCount);
+
+	temp = growthRate;
+
+	if (temp > 10)
+		temp = 10;
+	else if (temp < -3)
+		temp = -3;
+
+	if (temp < 0) {
+		temp = temp * -1;
+		alm->insertAttribute("plant_growth_rate_n", "@plant_grow:growth_loss_" + String::valueOf(temp));
+	} else {
+		alm->insertAttribute("plant_growth_rate_n", "@plant_grow:growth_rate_" + String::valueOf(temp));
+	}
+
+	temp = waterLevel;
+
+	if (temp > 100)
+		temp = 100;
+
+	alm->insertAttribute("plant_water_level_n", "@plant_grow:water_level_" + String::valueOf(temp / 10));
+
+	temp = nutrientLevel;
+
+	if (temp > 100)
+		temp = 100;
+
+	alm->insertAttribute("plant_nutrient_level_n", "@plant_grow:nutrient_level_" + String::valueOf(temp / 10));
+}
+
+void PlantObjectImplementation::sendResourceSUI(CreatureObject* player, int type) {
+	ManagedReference<SuiListBox*> suiBox = new SuiListBox(player, SuiWindowType::GROWABLE_PLANT, SuiListBox::HANDLETWOBUTTON);
+	suiBox->setCallback(new GrowablePlantSuiCallback(player->getZoneServer()));
+	suiBox->setPromptTitle("@plant_grow:select_resource_sub");
+	suiBox->setPromptText("@plant_grow:select_resource_body");
+	suiBox->setOkButton(true, "@ok");
+	suiBox->setCancelButton(true, "@cancel");
+	suiBox->setUsingObject(_this.getReferenceUnsafeStaticCast());
+
+	ManagedReference<SceneObject*> inventory = player->getSlottedObject("inventory");
+	ManagedReference<SceneObject*> sceneObject = NULL;
+
+	for (int i=0; i< inventory->getContainerObjectsSize(); i++) {
+		sceneObject = inventory->getContainerObject(i);
+
+		if (sceneObject == NULL)
+			continue;
+
+		if (sceneObject->isResourceContainer()) {
+			ManagedReference<ResourceContainer*> rcno = cast<ResourceContainer*>( sceneObject.get());
+
+			if (rcno == NULL)
+				continue;
+
+			ManagedReference<ResourceSpawn*> spawn = rcno->getSpawnObject();
+
+			if (spawn == NULL)
+				continue;
+
+			if ((type == 1 && spawn->isType("organic")) || (type == 2 && spawn->isType("water"))) {
+				suiBox->addMenuItem(spawn->getName(), sceneObject->getObjectID());
+			}
+		}
+	}
+
+	if (suiBox->getMenuSize() > 0) {
+		player->getPlayerObject()->addSuiBox(suiBox);
+		player->sendMessage(suiBox->generateMessage());
+	} else {
+		if (type == 1)
+			player->sendSystemMessage("@plant_grow:no_nutrients"); // You don't have any nutrients.
+		else
+			player->sendSystemMessage("@plant_grow:no_water"); // You don't have any water.
+	}
+}
+
+void PlantObjectImplementation::initializePlant(int size) {
+	lastPulse.updateToCurrentTime();
+
+	if (size == 1) {
+		idealWaterLevel = 30 + System::random(40);
+		idealNutrientLevel = 30 + System::random(40);
+	} else {
+		idealWaterLevel = 10 + System::random(80);
+		idealNutrientLevel = 10 + System::random(80);
+	}
+	criticalAttribOne = getCriticalAttribute(1 + System::random(6));
+	criticalAttribTwo = getCriticalAttribute(1 + System::random(6));
+
+	if (criticalAttribTwo == criticalAttribOne) {
+		while (criticalAttribTwo == criticalAttribOne)
+			criticalAttribTwo = getCriticalAttribute(1 + System::random(6));
+	}
+}
+
+void PlantObjectImplementation::changeSize(int size) {
+	String plantTemplate = "";
+
+	switch (size) {
+	case 0: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_dead.iff"; break;
+	case 1: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_1.iff"; break;
+	case 2: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_2.iff"; break;
+	case 3: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_3.iff"; break;
+	}
+
+	ManagedReference<ZoneServer*> zoneServer = getZoneServer();
+
+	if (zoneServer == NULL)
+		return;
+
+	ManagedReference<SceneObject*> parent = getParent();
+
+	if (parent == NULL || !parent->isCellObject())
+		return;
+
+	ManagedReference<SceneObject*> obj = zoneServer->createObject(plantTemplate.hashCode(), getPersistenceLevel());
+
+	if (obj == NULL)
+		return;
+
+
+	obj->initializePosition(getPositionX(), getPositionZ(), getPositionY());
+	obj->setDirection(Math::deg2rad(getDirectionAngle()));
+
+	parent->transferObject(obj, -1);
+
+	ManagedReference<PlantObject*> newPlant = cast<PlantObject*>( obj.get());
+
+	if (newPlant == NULL)
+		return;
+
+	newPlant->setWaterLevel(waterLevel);
+	newPlant->setNutrientLevel(nutrientLevel);
+	newPlant->setWaterQuality(waterQuality);
+	newPlant->setNutrientQuality(nutrientQuality);
+	newPlant->setPlantHealth(health);
+
+	newPlant->initializePlant(size);
+
+	destroyObjectFromWorld(true);
+	destroyObjectFromDatabase();
+}
+
+int PlantObjectImplementation::getCriticalAttribute(int index) {
+	switch (index) {
+	case 7: return CraftingManager::DR;
+	case 6: return CraftingManager::FL;
+	case 5: return CraftingManager::PE;
+	case 4: return CraftingManager::UT;
+	case 3: return CraftingManager::SR;
+	case 2: return CraftingManager::MA;
+	case 1:
+	default: return CraftingManager::OQ;
+	}
+}
+
+void PlantObjectImplementation::startPulse() {
+	if (pulseTask == NULL) {
+		pulseTask = new GrowablePlantPulseTask(_this.getReferenceUnsafeStaticCast());
+		pulseTask->reschedule(PULSERATE * 1000);
+	} else {
+		pulseTask->reschedule(PULSERATE * 1000);
+	}
+}

MMOCoreORB/src/server/zone/objects/tangible/tasks/GrowablePlantPulseTask.h
@@ -1,0 +1,141 @@
+#ifndef GROWABLEPLANTPULSETASK_H_
+#define GROWABLEPLANTPULSETASK_H_
+
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/tangible/misc/PlantObject.h"
+
+namespace server {
+namespace zone {
+namespace objects {
+namespace tangible {
+namespace tasks {
+
+class GrowablePlantPulseTask : public Task {
+	ManagedReference<PlantObject*> plant;
+
+public:
+	GrowablePlantPulseTask(PlantObject* pl) {
+		plant = pl;
+	}
+
+	void run() {
+		if (plant == NULL)
+			return;
+
+		Locker locker(plant);
+
+		Time currentTime;
+
+		Time* lastPulse = plant->getLastPulse();
+
+		// Cycles handles multiple pulses if server was offline during task trigger
+		int cycles = (currentTime.getMiliTime() - lastPulse->getMiliTime()) / (PlantObject::PULSERATE * 1000);
+
+		if (cycles < 1)
+			cycles = 1;
+		else if (cycles > 10)
+			cycles = 10;
+
+		ManagedReference<SceneObject*> parent = plant->getRootParent();
+
+		if (parent == NULL || !parent->isBuildingObject()) {
+			plant->updateLastPulse();
+			this->reschedule(PlantObject::PULSERATE * 1000);
+			return;
+		}
+
+		plant->updateLastPulse();
+
+		int waterDiff = plant->getWaterLevel() - plant->getIdealWaterLevel();
+		int nutrientDiff = plant->getNutrientLevel() - plant->getIdealNutrientLevel();
+
+		if (waterDiff < 20 && nutrientDiff < 20) {
+			int healthUpdate = (((40 - (waterDiff + waterDiff)) * (plant->getWaterQuality() + plant->getNutrientQuality()) / 2000) / 5) * cycles;
+
+			if (healthUpdate < 1)
+				healthUpdate = 1;
+
+			int health = plant->getPlantHealth() + healthUpdate;
+
+			if (health > 100)
+				health = 100;
+
+			plant->setPlantHealth(health);
+			plant->setGrowthRate(healthUpdate / cycles);
+
+			if (health > 70) {
+				int healthPerCycle = healthUpdate / cycles;
+				int growthUpdate = (((health / 20) + healthPerCycle) * (plant->getWaterQuality() + plant->getNutrientQuality()) / 2000) * cycles;
+
+				plant->setGrowth(plant->getGrowth() + growthUpdate);
+			}
+		} else {
+			if (waterDiff < 20)
+				waterDiff = 20;
+			if (nutrientDiff < 20)
+				nutrientDiff = 20;
+
+			int healthUpdate = (((waterDiff - 20) + (nutrientDiff - 20)) / 10) * cycles;
+
+			if (healthUpdate > 6)
+				healthUpdate = 6;
+			if (healthUpdate < 1)
+				healthUpdate = 1;
+
+			int health = plant->getPlantHealth() - healthUpdate;
+			plant->setPlantHealth(health);
+
+			int healthPerCycle = healthUpdate / cycles;
+			int growthRate = healthPerCycle / 2;
+
+			if (growthRate < 1)
+				growthRate = 1;
+
+			plant->setGrowthRate(growthRate * -1);
+
+			if (health < 1) {
+				plant->changeSize(0);
+				return;
+			}
+		}
+
+		int nutrientLevel = plant->getNutrientLevel() - cycles;
+
+		if (nutrientLevel < 0)
+			nutrientLevel = 0;
+
+		int waterLevel = plant->getWaterLevel() - cycles;
+
+		if (waterLevel < 0)
+			waterLevel = 0;
+
+		plant->setNutrientLevel(nutrientLevel);
+		plant->setWaterLevel(waterLevel);
+
+		if (plant->getGrowth() > 100) {
+			if (plant->getSize() < 3) {
+				plant->setGrowth(0);
+				plant->changeSize(plant->getSize() + 1);
+				return;
+			} else {
+				plant->setGrowth(0);
+				int fruit = plant->getFruit();
+
+				if (fruit < 10)
+					plant->setFruit(fruit + 1);
+			}
+		}
+
+		this->reschedule(PlantObject::PULSERATE * 1000);
+	}
+};
+
+}
+}
+}
+}
+}
+
+using namespace server::zone::objects::tangible::tasks;
+
+#endif /* GROWABLEPLANTPULSETASK_H_ */

TheTinyPebble/swgtcw-test

MMOCoreORB/src/server/zone/objects/tangible/misc/PlantObjectImplementation.cpp
@@ -1,0 +1,291 @@
+#include "server/zone/objects/tangible/misc/PlantObject.h"
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/player/PlayerObject.h"
+#include "server/zone/packets/scene/AttributeListMessage.h"
+#include "server/zone/packets/object/ObjectMenuResponse.h"
+#include "server/zone/objects/player/sui/listbox/SuiListBox.h"
+#include "server/zone/objects/player/sui/callbacks/GrowablePlantSuiCallback.h"
+#include "server/zone/managers/crafting/CraftingManager.h"
+#include "server/zone/objects/resource/ResourceContainer.h"
+#include "server/zone/objects/building/BuildingObject.h"
+#include "server/zone/objects/tangible/tasks/GrowablePlantPulseTask.h"
+#include "server/zone/Zone.h"
+#include "server/zone/ZoneServer.h"
+
+void PlantObjectImplementation::fillObjectMenuResponse(ObjectMenuResponse* menuResponse, CreatureObject* player) {
+	TangibleObjectImplementation::fillObjectMenuResponse(menuResponse, player);
+
+	if (plantSize == 0)
+		return;
+
+	menuResponse->addRadialMenuItem(68, 3, "@plant_grow:nutrients");
+	menuResponse->addRadialMenuItemToRadialID(68, 69, 3, "@plant_grow:add_nutrients");
+	menuResponse->addRadialMenuItemToRadialID(68, 70, 3, "@plant_grow:remove_nutrients_menu");
+
+	menuResponse->addRadialMenuItem(71, 3, "@plant_grow:water");
+	menuResponse->addRadialMenuItemToRadialID(71, 72, 3, "@plant_grow:add_water");
+	menuResponse->addRadialMenuItemToRadialID(71, 73, 3, "@plant_grow:remove_water_menu");
+
+	if (fruitCount > 0)
+		menuResponse->addRadialMenuItem(74, 3, "@plant_grow:pick_fruit_menu");
+}
+
+int PlantObjectImplementation::handleObjectMenuSelect(CreatureObject* player, byte selectedID) {
+	ManagedReference<SceneObject*> rootParent = getRootParent();
+	ManagedReference<SceneObject*> parent = getParent();
+
+	if (rootParent == NULL || parent == NULL || !rootParent->isBuildingObject() || !parent->isCellObject()) {
+		player->sendSystemMessage("@plant_grow:must_be_in_building"); // The plant must be in a building which you administrate.
+		return 0;
+	}
+
+	ManagedReference<BuildingObject*> building = cast<BuildingObject*>( rootParent.get());
+
+	if (!building->isOnAdminList(player) && selectedID >= 69 && selectedID <= 74) {
+		player->sendSystemMessage("@plant_grow:must_be_in_building"); // The plant must be in a building which you administrate.
+		return 0;
+	}
+
+	if (selectedID == 69) { // Add Nutrients
+		sendResourceSUI(player, 1);
+	} else if (selectedID == 72) { // Add Water
+		sendResourceSUI(player, 2);
+	} else if (selectedID == 70) { // Remove Nutrients
+		nutrientLevel -= 5;
+
+		if (nutrientLevel < 0)
+			nutrientLevel = 0;
+
+		player->sendSystemMessage("@plant_grow:remove_nutrients");
+	} else if (selectedID == 73) { // Remove Water
+		waterLevel -= 5;
+
+		if (waterLevel < 0)
+			waterLevel = 0;
+
+		player->sendSystemMessage("@plant_grow:remove_water");
+	} else if (selectedID == 74) { // Pick Fruit
+		if (fruitCount < 1)
+			return 0;
+
+		ManagedReference<SceneObject*> inventory = player->getSlottedObject("inventory");
+
+		if(inventory->isContainerFullRecursive()){
+			player->sendSystemMessage("@plant_grow:no_inventory"); // You do not have any inventory space.
+			return 0;
+		}
+
+		ZoneServer* zserv = player->getZoneServer();
+
+		fruitCount--;
+
+		String fruitTemplate = "object/tangible/item/plant/force_melon.iff";
+		Reference<SceneObject*> fruit = zserv->createObject(fruitTemplate.hashCode(), 1);
+
+		if(fruit == NULL) {
+			return 0;
+		}
+
+		if (!inventory->transferObject(fruit, -1, false, false) ) {
+			fruit->destroyObjectFromDatabase(true);
+			return 0;
+		}
+
+		inventory->broadcastObject(fruit, true);
+		player->sendSystemMessage("@plant_grow:pick_fruit"); // You pick a piece of fruit from the plant.
+
+	}
+
+	return 1;
+}
+
+void PlantObjectImplementation::fillAttributeList(AttributeListMessage* alm, CreatureObject* object) {
+	int temp = health;
+
+	if (temp > 0 && temp < 10)
+		temp = 10;
+	alm->insertAttribute("plant_health_n", "@plant_grow:health_" + String::valueOf(temp / 10));
+
+	if (fruitCount > 0)
+		alm->insertAttribute("plant_fruit", fruitCount);
+
+	temp = growthRate;
+
+	if (temp > 10)
+		temp = 10;
+	else if (temp < -3)
+		temp = -3;
+
+	if (temp < 0) {
+		temp = temp * -1;
+		alm->insertAttribute("plant_growth_rate_n", "@plant_grow:growth_loss_" + String::valueOf(temp));
+	} else {
+		alm->insertAttribute("plant_growth_rate_n", "@plant_grow:growth_rate_" + String::valueOf(temp));
+	}
+
+	temp = waterLevel;
+
+	if (temp > 100)
+		temp = 100;
+
+	alm->insertAttribute("plant_water_level_n", "@plant_grow:water_level_" + String::valueOf(temp / 10));
+
+	temp = nutrientLevel;
+
+	if (temp > 100)
+		temp = 100;
+
+	alm->insertAttribute("plant_nutrient_level_n", "@plant_grow:nutrient_level_" + String::valueOf(temp / 10));
+}
+
+void PlantObjectImplementation::sendResourceSUI(CreatureObject* player, int type) {
+	ManagedReference<SuiListBox*> suiBox = new SuiListBox(player, SuiWindowType::GROWABLE_PLANT, SuiListBox::HANDLETWOBUTTON);
+	suiBox->setCallback(new GrowablePlantSuiCallback(player->getZoneServer()));
+	suiBox->setPromptTitle("@plant_grow:select_resource_sub");
+	suiBox->setPromptText("@plant_grow:select_resource_body");
+	suiBox->setOkButton(true, "@ok");
+	suiBox->setCancelButton(true, "@cancel");
+	suiBox->setUsingObject(_this.getReferenceUnsafeStaticCast());
+	suiBox->setForceCloseDistance(32.f);
+
+	ManagedReference<SceneObject*> inventory = player->getSlottedObject("inventory");
+	ManagedReference<SceneObject*> sceneObject = NULL;
+
+	ManagedReference<PlayerObject*> ghost = player->getPlayerObject();
+
+	if (ghost == NULL)
+		return;
+
+	for (int i=0; i< inventory->getContainerObjectsSize(); i++) {
+		sceneObject = inventory->getContainerObject(i);
+
+		if (sceneObject == NULL)
+			continue;
+
+		if (sceneObject->isResourceContainer()) {
+			ManagedReference<ResourceContainer*> rcno = cast<ResourceContainer*>( sceneObject.get());
+
+			if (rcno == NULL)
+				continue;
+
+			ManagedReference<ResourceSpawn*> spawn = rcno->getSpawnObject();
+
+			if (spawn == NULL)
+				continue;
+
+			if ((type == 1 && spawn->isType("organic")) || (type == 2 && spawn->isType("water"))) {
+				suiBox->addMenuItem(spawn->getName(), sceneObject->getObjectID());
+			}
+		}
+	}
+
+	if (suiBox->getMenuSize() > 0) {
+		ghost->closeSuiWindowType(SuiWindowType::GROWABLE_PLANT);
+		player->getPlayerObject()->addSuiBox(suiBox);
+		player->sendMessage(suiBox->generateMessage());
+	} else {
+		if (type == 1)
+			player->sendSystemMessage("@plant_grow:no_nutrients"); // You don't have any nutrients.
+		else
+			player->sendSystemMessage("@plant_grow:no_water"); // You don't have any water.
+	}
+}
+
+void PlantObjectImplementation::initializePlant(int size) {
+	lastPulse.updateToCurrentTime();
+
+	if (size == 1) {
+		idealWaterLevel = 30 + System::random(40);
+		idealNutrientLevel = 30 + System::random(40);
+	} else {
+		idealWaterLevel = 10 + System::random(80);
+		idealNutrientLevel = 10 + System::random(80);
+	}
+	criticalAttribOne = getCriticalAttribute(1 + System::random(6));
+	criticalAttribTwo = getCriticalAttribute(1 + System::random(6));
+
+	if (criticalAttribTwo == criticalAttribOne) {
+		while (criticalAttribTwo == criticalAttribOne)
+			criticalAttribTwo = getCriticalAttribute(1 + System::random(6));
+	}
+
+}
+
+void PlantObjectImplementation::changeSize(int size) {
+	String plantTemplate = "";
+
+	switch (size) {
+	case 0: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_dead.iff"; break;
+	case 1: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_1.iff"; break;
+	case 2: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_2.iff"; break;
+	case 3: plantTemplate = "object/tangible/loot/plant_grow/plant_stage_3.iff"; break;
+	}
+
+	ManagedReference<ZoneServer*> zoneServer = getZoneServer();
+
+	if (zoneServer == NULL)
+		return;
+
+	ManagedReference<SceneObject*> parent = getParent();
+
+	if (parent == NULL || !parent->isCellObject())
+		return;
+
+	ManagedReference<SceneObject*> obj = zoneServer->createObject(plantTemplate.hashCode(), getPersistenceLevel());
+
+	if (obj == NULL)
+		return;
+
+	Locker clocker(obj, _this.getReferenceUnsafeStaticCast());
+
+	obj->initializePosition(getPositionX(), getPositionZ(), getPositionY());
+	obj->setDirection(Math::deg2rad(getDirectionAngle()));
+
+	ManagedReference<PlantObject*> newPlant = cast<PlantObject*>( obj.get());
+
+	if (newPlant == NULL)
+		return;
+
+	newPlant->setWaterLevel(waterLevel);
+	newPlant->setNutrientLevel(nutrientLevel);
+	newPlant->setWaterQuality(waterQuality);
+	newPlant->setNutrientQuality(nutrientQuality);
+	newPlant->setPlantHealth(health);
+
+	newPlant->initializePlant(size);
+
+	parent->transferObject(obj, -1);
+
+	clocker.release();
+
+	destroyObjectFromWorld(true);
+	destroyObjectFromDatabase();
+}
+
+int PlantObjectImplementation::getCriticalAttribute(int index) {
+	switch (index) {
+	case 7: return CraftingManager::DR;
+	case 6: return CraftingManager::FL;
+	case 5: return CraftingManager::PE;
+	case 4: return CraftingManager::UT;
+	case 3: return CraftingManager::SR;
+	case 2: return CraftingManager::MA;
+	case 1:
+	default: return CraftingManager::OQ;
+	}
+}
+
+void PlantObjectImplementation::startPulse() {
+	Time currentTime;
+	int timeSinceLast = currentTime.getMiliTime() - lastPulse.getMiliTime();
+
+	if (pulseTask == NULL)
+		pulseTask = new GrowablePlantPulseTask(_this.getReferenceUnsafeStaticCast());
+
+	if (timeSinceLast >= (PULSERATE * 1000))
+		pulseTask->execute();
+	else if (pulseTask->isScheduled())
+		pulseTask->reschedule((PULSERATE * 1000) - timeSinceLast);
+	else
+		pulseTask->schedule((PULSERATE * 1000) - timeSinceLast);
+}

MMOCoreORB/src/server/zone/objects/tangible/tasks/GrowablePlantPulseTask.h
@@ -1,0 +1,143 @@
+#ifndef GROWABLEPLANTPULSETASK_H_
+#define GROWABLEPLANTPULSETASK_H_
+
+#include "server/zone/objects/creature/CreatureObject.h"
+#include "server/zone/objects/tangible/misc/PlantObject.h"
+
+namespace server {
+namespace zone {
+namespace objects {
+namespace tangible {
+namespace tasks {
+
+class GrowablePlantPulseTask : public Task {
+	ManagedWeakReference<PlantObject*> plant;
+
+public:
+	GrowablePlantPulseTask(PlantObject* pl) {
+		plant = pl;
+	}
+
+	void run() {
+		ManagedReference<PlantObject*> strongRef = plant.get();
+
+		if (strongRef == NULL)
+			return;
+
+		Locker locker(strongRef);
+
+		Time currentTime;
+
+		Time* lastPulse = strongRef->getLastPulse();
+
+		// Cycles handles multiple pulses if server was offline during task trigger
+		int cycles = (currentTime.getMiliTime() - lastPulse->getMiliTime()) / (PlantObject::PULSERATE * 1000);
+
+		if (cycles < 1)
+			cycles = 1;
+		else if (cycles > 10)
+			cycles = 10;
+
+		ManagedReference<SceneObject*> rootParent = strongRef->getRootParent();
+		ManagedReference<SceneObject*> parent = strongRef->getParent();
+
+		strongRef->updateLastPulse();
+
+		if (parent == NULL || rootParent == NULL || !rootParent->isBuildingObject() || !parent->isCellObject()) {
+			this->reschedule(PlantObject::PULSERATE * 1000);
+			return;
+		}
+
+		int waterDiff = abs(strongRef->getWaterLevel() - strongRef->getIdealWaterLevel());
+		int nutrientDiff = abs(strongRef->getNutrientLevel() - strongRef->getIdealNutrientLevel());
+
+		if (waterDiff < 20 && nutrientDiff < 20) {
+			int healthUpdate = (((40 - (waterDiff + nutrientDiff)) * (strongRef->getWaterQuality() + strongRef->getNutrientQuality()) / 2000) / 5) * cycles;
+
+			if (healthUpdate < 1)
+				healthUpdate = 1;
+
+			int health = strongRef->getPlantHealth() + healthUpdate;
+
+			if (health > 100)
+				health = 100;
+
+			strongRef->setPlantHealth(health);
+			strongRef->setGrowthRate(healthUpdate / cycles);
+
+			if (health > 70) {
+				int healthPerCycle = healthUpdate / cycles;
+				int growthUpdate = (((health / 20) + healthPerCycle) * (strongRef->getWaterQuality() + strongRef->getNutrientQuality()) / 2000) * cycles;
+
+				strongRef->setGrowth(strongRef->getGrowth() + growthUpdate);
+			}
+		} else {
+			if (waterDiff < 20)
+				waterDiff = 20;
+			if (nutrientDiff < 20)
+				nutrientDiff = 20;
+
+			int healthUpdate = (((waterDiff - 20) + (nutrientDiff - 20)) / 10) * cycles;
+
+			if (healthUpdate > 6)
+				healthUpdate = 6;
+			if (healthUpdate < 1)
+				healthUpdate = 1;
+
+			int health = strongRef->getPlantHealth() - healthUpdate;
+			strongRef->setPlantHealth(health);
+
+			int healthPerCycle = healthUpdate / cycles;
+			int growthRate = healthPerCycle / 2;
+
+			if (growthRate < 1)
+				growthRate = 1;
+
+			strongRef->setGrowthRate(growthRate * -1);
+
+			if (health < 1) {
+				strongRef->changeSize(0);
+				return;
+			}
+		}
+
+		int nutrientLevel = strongRef->getNutrientLevel() - cycles;
+
+		if (nutrientLevel < 0)
+			nutrientLevel = 0;
+
+		int waterLevel = strongRef->getWaterLevel() - cycles;
+
+		if (waterLevel < 0)
+			waterLevel = 0;
+
+		strongRef->setNutrientLevel(nutrientLevel);
+		strongRef->setWaterLevel(waterLevel);
+
+		if (strongRef->getGrowth() > 100) {
+			if (strongRef->getSize() < 3) {
+				strongRef->setGrowth(0);
+				strongRef->changeSize(strongRef->getSize() + 1);
+				return;
+			} else {
+				strongRef->setGrowth(0);
+				int fruit = strongRef->getFruit();
+
+				if (fruit < 10)
+					strongRef->setFruit(fruit + 1);
+			}
+		}
+
+		this->reschedule(PlantObject::PULSERATE * 1000);
+	}
+};
+
+}
+}
+}
+}
+}
+
+using namespace server::zone::objects::tangible::tasks;
+
+#endif /* GROWABLEPLANTPULSETASK_H_ */