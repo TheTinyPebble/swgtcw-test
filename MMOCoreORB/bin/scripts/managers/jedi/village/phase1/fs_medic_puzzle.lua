swgemu/Core3

[added] Sivarra's Village Phase 1 medic questline [removed] Free branch unlock from Paemos
Change-Id: I2c33f8f38b6c860bfcdbcf76aec373b0d4e6ca04

MMOCoreORB/bin/scripts/managers/jedi/village/phase1/fs_medic_puzzle.lua
@@ -1,0 +1,155 @@
+local ObjectManager = require("managers.object.object_manager")
+local QuestManager = require("managers.quest.quest_manager")
+
+FsMedicPuzzle = Object:new {
+	totalSymptoms = 12
+}
+
+function FsMedicPuzzle:giveSymptoms(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	local maxSymptoms = getRandomNumber(3, self.totalSymptoms - 3)
+	for i = 1, maxSymptoms, 1 do
+		local symptomNum = getRandomNumber(0, self.totalSymptoms - 1)
+
+		while self:hasSpecificSymptom(pPlayer, pNpc, symptomNum) do
+			symptomNum = getRandomNumber(0, self.totalSymptoms - 1)
+		end
+
+		writeData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. symptomNum, 1)
+	end
+end
+
+function FsMedicPuzzle:hasSpecificSymptom(pPlayer, pNpc, symptom)
+	return readData(SceneObject(pPlayer):getObjectID() .. ":fs_medic_puzzle_npc:" .. SceneObject(pNpc):getObjectID() .. ":symptom" .. symptom) == 1
+end
+
+function FsMedicPuzzle:hasAnySymptoms(pPlayer, pNpc)
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if (readData(SceneObject(pPlayer):getObjectID() .. ":fs_medic_puzzle_npc:" .. SceneObject(pNpc):getObjectID() .. ":symptom" .. i) == 1) then
+			return true
+		end
+	end
+
+	return false
+end
+
+function FsMedicPuzzle:cleanupNpc(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	for i = 0, self.totalSymptoms - 1, 1 do
+		deleteData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. i)
+	end
+end
+
+function FsMedicPuzzle:getSymptoms(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	local numSymptoms = 0
+	local symptomString = ""
+	local lastSymptom = self:getLastSymptom(pPlayer, pNpc)
+
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if self:hasSpecificSymptom(pPlayer, pNpc, i) then
+			numSymptoms = numSymptoms + 1
+			if (numSymptoms == 1) then
+				symptomString = getStringId("@quest/force_sensitive/fs_medic:symptom" .. i)
+			else
+				if (i == lastSymptom) then
+					symptomString = symptomString .. " and " .. getStringId("@quest/force_sensitive/fs_medic:symptom" .. i)
+				else
+					symptomString = symptomString .. ", " .. getStringId("@quest/force_sensitive/fs_medic:symptom" .. i)
+				end
+			end
+		end
+	end
+
+	return symptomString
+end
+
+function FsMedicPuzzle:getCuredVillagerCount(pPlayer)
+	local count = QuestManager.getStoredVillageValue(pPlayer, "FsMedicPuzzleVillagerCount")
+
+	if (count == "") then
+		count = 0
+	end
+
+	return tonumber(count)
+end
+
+function FsMedicPuzzle:setCuredVillagerCount(pPlayer, count)
+	QuestManager.setStoredVillageValue(pPlayer, "FsMedicPuzzleVillagerCount", count)
+end
+
+function FsMedicPuzzle:getPatientsLeftToTreat(pPlayer)
+	local count = self:getCuredVillagerCount(pPlayer)
+	if QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03) then
+		return 15 - count
+	elseif QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) then
+		return 10 - count
+	elseif QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01) then
+		return 5 - count
+	end
+end
+
+function FsMedicPuzzle:getSymptomCount(pPlayer, pNpc)
+	local symptomCount = 0
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if self:hasSpecificSymptom(pPlayer, pNpc, i) then
+			symptomCount = symptomCount + 1
+		end
+	end
+	return symptomCount
+end
+
+function FsMedicPuzzle:getLastSymptom(pPlayer, pNpc)
+	local lastSymptom = -1
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if self:hasSpecificSymptom(pPlayer, pNpc, i) then
+			lastSymptom = i
+		end
+	end
+	return lastSymptom
+end
+
+function FsMedicPuzzle:cureSymptoms(pPlayer, pNpc, pPack)
+	local puzzlePack = LuaFsPuzzlePack(pPack)
+	local cureSymptomOne = puzzlePack:getCureSymptomOne()
+	local cureSymptomTwo = puzzlePack:getCureSymptomTwo()
+	local giveSymptomOne = puzzlePack:getGiveSymptomOne()
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+
+	spatialChat(pNpc, "@quest/force_sensitive/fs_medic:admin_meds")
+	if (cureSymptomOne >= 0) then
+		deleteData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. cureSymptomOne)
+	end
+
+	if (cureSymptomTwo >= 0) then
+		deleteData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. cureSymptomTwo)
+	end
+
+	if (giveSymptomOne >= 0) then
+		writeData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. giveSymptomOne, 1)
+	end
+
+	local symptomCount = self:getSymptomCount(pPlayer, pNpc)
+
+	if (symptomCount == 0) then
+		CreatureObject(pPlayer):sendSystemMessage("@quest/force_sensitive/fs_medic:npc_cured")
+		local villagerCount = self:getCuredVillagerCount(pPlayer)
+		self:setCuredVillagerCount(pPlayer, villagerCount + 1)
+	end
+
+	SceneObject(pPack):destroyObjectFromWorld()
+	SceneObject(pPack):destroyObjectFromDatabase()
+end
+
+function FsMedicPuzzle:cleanUp(pPlayer)
+
+	ObjectManager.withCreaturePlayerObject(pPlayer, function(playerObject)
+		playerObject:removeRewardedSchematic("object/draft_schematic/item/quest_item/fs_medic_puzzle_heal_pack.iff", true)
+	end)
+end
+
+return FsMedicPuzzle

MMOCoreORB/bin/scripts/screenplays/village/convos/phase1/sivarra_phase1_conv_handler.lua
@@ -1,0 +1,117 @@
+local ObjectManager = require("managers.object.object_manager")
+local VillageJediManagerCommon = require("managers.jedi.village.village_jedi_manager_common")
+local FsMedicPuzzle = require("managers.jedi.village.phase1.fs_medic_puzzle")
+local QuestManager = require("managers.quest.quest_manager")
+
+villageSivarraPhase1ConvoHandler = {  }
+
+function villageSivarraPhase1ConvoHandler:getInitialScreen(pPlayer, pNpc, pConversationTemplate)
+	local convoTemplate = LuaConversationTemplate(pConversationTemplate)
+
+	if (VillageJediManagerTownship:getCurrentPhase() ~= 1) then
+		return convoTemplate:getScreen("intro_cant_talk")
+	elseif (QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_FINISH)) then
+		return convoTemplate:getScreen("intro_did_all_sets")
+	end
+
+	local pInventory = SceneObject(pPlayer):getSlottedObject("inventory")
+
+	if (pInventory == nil or SceneObject(pInventory):isContainerFullRecursive()) then
+		return convoTemplate:getScreen("intro_inv_full")
+	end
+
+	local curedCount = FsMedicPuzzle:getCuredVillagerCount(pPlayer)
+	if (curedCount >= 15 and QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03)) then
+		return convoTemplate:getScreen("intro_completed_third_set")
+	elseif (curedCount >= 10 and QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)) then
+		return convoTemplate:getScreen("intro_completed_second_set")
+	elseif (curedCount >= 5 and QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)) then
+		return convoTemplate:getScreen("intro_completed_first_set")
+	elseif ((QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03) and curedCount < 15) or
+		(QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) and curedCount < 10) or
+		(QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01) and curedCount < 5)) then
+		return convoTemplate:getScreen("intro_in_progress")
+	elseif (not QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03) and
+		QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)) then
+		return convoTemplate:getScreen("intro_start_third_set")
+	elseif (not QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) and
+		QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)) then
+		return convoTemplate:getScreen("intro_start_second_set")
+	elseif (not CreatureObject(pPlayer):hasSkill("science_medic_master")) then
+		convoTemplate:getScreen("intro_not_master_medic")
+	end
+
+	return convoTemplate:getScreen("intro")
+end
+
+function villageSivarraPhase1ConvoHandler:runScreenHandlers(conversationTemplate, conversingPlayer, conversingNPC, selectedOption, conversationScreen)
+	local screen = LuaConversationScreen(conversationScreen)
+	local screenID = screen:getScreenID()
+	local conversationScreen = screen:cloneScreen()
+	local clonedConversation = LuaConversationScreen(conversationScreen)
+
+	if (screenID == "great_explain") then
+		QuestManager.setCurrentQuestID(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)
+		QuestManager.activateQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)
+		FsMedicPuzzle:setCuredVillagerCount(conversingPlayer, 0)
+
+		ObjectManager.withCreaturePlayerObject(conversingPlayer, function(playerObject)
+			playerObject:addRewardedSchematic("object/draft_schematic/item/quest_item/fs_medic_puzzle_heal_pack.iff", 2, -1, true)
+		end)
+	elseif (screenID == "intro_completed_first_set") then
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)
+		VillageJediManagerCommon.unlockBranch(conversingPlayer, "force_sensitive_heightened_senses_persuasion")
+	elseif (screenID == "intro_completed_second_set") then
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)
+
+		local pInventory = SceneObject(conversingPlayer):getSlottedObject("inventory")
+
+		if (pInventory ~= nil) then
+			local pPole = giveItem(pInventory, "object/tangible/wearables/necklace/necklace_ice_pendant.iff", -1, true)
+
+			if (pPole == nil) then
+				CreatureObject(conversingPlayer):sendSystemMessage("Error: Unable to generate item.")
+			end
+		end
+	elseif (screenID == "intro_completed_third_set") then
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03)
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_FINISH)
+
+		local pInventory = SceneObject(conversingPlayer):getSlottedObject("inventory")
+
+		if (pInventory ~= nil) then
+			local pPole = giveItem(pInventory, "object/tangible/loot/plant_grow/plant_stage_1.iff", -1, true)
+
+			if (pPole == nil) then
+				CreatureObject(conversingPlayer):sendSystemMessage("Error: Unable to generate item.")
+			end
+		end
+
+		FsMedicPuzzle:cleanUp(conversingPlayer)
+	elseif (screenID == "talk_droids_again") then
+		QuestManager.activateQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)
+	elseif (screenID == "talk_droids_round_three") then
+		QuestManager.activateQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)
+	elseif (screenID == "intro_in_progress") then
+		local count = tostring(FsMedicPuzzle:getPatientsLeftToTreat(conversingPlayer))
+		clonedConversation:setDialogTextTO(count)
+	end
+
+	return conversationScreen
+end
+
+function villageSivarraPhase1ConvoHandler:getNextConversationScreen(pConversationTemplate, pPlayer, selectedOption, pConversingNpc)
+	local pConversationSession = CreatureObject(pPlayer):getConversationSession()
+	local pLastConversationScreen = nil
+	if (pConversationSession ~= nil) then
+		local conversationSession = LuaConversationSession(pConversationSession)
+		pLastConversationScreen = conversationSession:getLastConversationScreen()
+	end
+	local conversationTemplate = LuaConversationTemplate(pConversationTemplate)
+	if (pLastConversationScreen ~= nil) then
+		local lastConversationScreen = LuaConversationScreen(pLastConversationScreen)
+		local optionLink = lastConversationScreen:getOptionLink(selectedOption)
+		return conversationTemplate:getScreen(optionLink)
+	end
+	return self:getInitialScreen(pPlayer, pConversingNpc, pConversationTemplate)
+end

TheTinyPebble/swgtcw-test

MMOCoreORB/bin/scripts/managers/jedi/village/phase1/fs_medic_puzzle.lua
@@ -1,0 +1,165 @@
+local ObjectManager = require("managers.object.object_manager")
+local QuestManager = require("managers.quest.quest_manager")
+
+FsMedicPuzzle = Object:new {
+	totalSymptoms = 12
+}
+
+function FsMedicPuzzle:giveSymptoms(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	local maxSymptoms = getRandomNumber(3, self.totalSymptoms - 3)
+	for i = 1, maxSymptoms, 1 do
+		local symptomNum = getRandomNumber(0, self.totalSymptoms - 1)
+
+		while self:hasSpecificSymptom(playerID, npcID, symptomNum) do
+			symptomNum = getRandomNumber(0, self.totalSymptoms - 1)
+		end
+
+		writeData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. symptomNum, 1)
+	end
+end
+
+function FsMedicPuzzle:hasSpecificSymptom(playerID, npcID, symptom)
+	return readData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. symptom) == 1
+end
+
+function FsMedicPuzzle:hasAnySymptoms(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if self:hasSpecificSymptom(playerID, npcID, i) then
+			return true
+		end
+	end
+
+	return false
+end
+
+function FsMedicPuzzle:cleanupNpc(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	for i = 0, self.totalSymptoms - 1, 1 do
+		deleteData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. i)
+	end
+end
+
+function FsMedicPuzzle:getSymptoms(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	local numSymptoms = 0
+	local symptomString = ""
+	local lastSymptom = self:getLastSymptom(pPlayer, pNpc)
+
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if self:hasSpecificSymptom(playerID, npcID, i) then
+			numSymptoms = numSymptoms + 1
+			if (numSymptoms == 1) then
+				symptomString = getStringId("@quest/force_sensitive/fs_medic:symptom" .. i)
+			else
+				if (i == lastSymptom) then
+					symptomString = symptomString .. " and " .. getStringId("@quest/force_sensitive/fs_medic:symptom" .. i)
+				else
+					symptomString = symptomString .. ", " .. getStringId("@quest/force_sensitive/fs_medic:symptom" .. i)
+				end
+			end
+		end
+	end
+
+	return symptomString
+end
+
+function FsMedicPuzzle:getCuredVillagerCount(pPlayer)
+	local count = QuestManager.getStoredVillageValue(pPlayer, "FsMedicPuzzleVillagerCount")
+
+	if (count == "") then
+		count = 0
+	end
+
+	return tonumber(count)
+end
+
+function FsMedicPuzzle:setCuredVillagerCount(pPlayer, count)
+	QuestManager.setStoredVillageValue(pPlayer, "FsMedicPuzzleVillagerCount", count)
+end
+
+function FsMedicPuzzle:getPatientsLeftToTreat(pPlayer)
+	local count = self:getCuredVillagerCount(pPlayer)
+	if QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03) then
+		return 15 - count
+	elseif QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) then
+		return 10 - count
+	elseif QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01) then
+		return 5 - count
+	end
+end
+
+function FsMedicPuzzle:getSymptomCount(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	local symptomCount = 0
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if self:hasSpecificSymptom(playerID, npcID, i) then
+			symptomCount = symptomCount + 1
+		end
+	end
+	return symptomCount
+end
+
+function FsMedicPuzzle:getLastSymptom(pPlayer, pNpc)
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+	local lastSymptom = -1
+	for i = 0, self.totalSymptoms - 1, 1 do
+		if self:hasSpecificSymptom(playerID, npcID, i) then
+			lastSymptom = i
+		end
+	end
+	return lastSymptom
+end
+
+function FsMedicPuzzle:cureSymptoms(pPlayer, pNpc, pPack)
+	local puzzlePack = LuaFsPuzzlePack(pPack)
+	local cureSymptomOne = puzzlePack:getCureSymptomOne()
+	local cureSymptomTwo = puzzlePack:getCureSymptomTwo()
+	local giveSymptomOne = puzzlePack:getGiveSymptomOne()
+	local playerID = SceneObject(pPlayer):getObjectID()
+	local npcID = SceneObject(pNpc):getObjectID()
+
+	spatialChat(pNpc, "@quest/force_sensitive/fs_medic:admin_meds")
+	if (cureSymptomOne >= 0) then
+		deleteData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. cureSymptomOne)
+	end
+
+	if (cureSymptomTwo >= 0) then
+		deleteData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. cureSymptomTwo)
+	end
+
+	if (giveSymptomOne >= 0) then
+		writeData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. giveSymptomOne, 1)
+	end
+
+	local symptomCount = self:getSymptomCount(pPlayer, pNpc)
+
+	if (symptomCount == 0) then
+		CreatureObject(pPlayer):sendSystemMessage("@quest/force_sensitive/fs_medic:npc_cured")
+		local villagerCount = self:getCuredVillagerCount(pPlayer)
+		self:setCuredVillagerCount(pPlayer, villagerCount + 1)
+		
+		for i = 0, self.totalSymptoms - 1, 1 do
+			deleteData(playerID .. ":fs_medic_puzzle_npc:" .. npcID .. ":symptom" .. i)
+		end
+	end
+
+	SceneObject(pPack):destroyObjectFromWorld()
+	SceneObject(pPack):destroyObjectFromDatabase()
+end
+
+function FsMedicPuzzle:cleanUp(pPlayer)
+
+	ObjectManager.withCreaturePlayerObject(pPlayer, function(playerObject)
+		playerObject:removeRewardedSchematic("object/draft_schematic/item/quest_item/fs_medic_puzzle_heal_pack.iff", true)
+	end)
+end
+
+return FsMedicPuzzle

MMOCoreORB/bin/scripts/screenplays/village/convos/phase1/sivarra_phase1_conv_handler.lua
@@ -1,0 +1,117 @@
+local ObjectManager = require("managers.object.object_manager")
+local VillageJediManagerCommon = require("managers.jedi.village.village_jedi_manager_common")
+local FsMedicPuzzle = require("managers.jedi.village.phase1.fs_medic_puzzle")
+local QuestManager = require("managers.quest.quest_manager")
+
+villageSivarraPhase1ConvoHandler = {  }
+
+function villageSivarraPhase1ConvoHandler:getInitialScreen(pPlayer, pNpc, pConversationTemplate)
+	local convoTemplate = LuaConversationTemplate(pConversationTemplate)
+
+	if (VillageJediManagerTownship:getCurrentPhase() ~= 1) then
+		return convoTemplate:getScreen("intro_cant_talk")
+	elseif (QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_FINISH)) then
+		return convoTemplate:getScreen("intro_did_all_sets")
+	end
+
+	local pInventory = SceneObject(pPlayer):getSlottedObject("inventory")
+
+	if (pInventory == nil or SceneObject(pInventory):isContainerFullRecursive()) then
+		return convoTemplate:getScreen("intro_inv_full")
+	end
+
+	local curedCount = FsMedicPuzzle:getCuredVillagerCount(pPlayer)
+	if (curedCount >= 15 and QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03)) then
+		return convoTemplate:getScreen("intro_completed_third_set")
+	elseif (curedCount >= 10 and QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)) then
+		return convoTemplate:getScreen("intro_completed_second_set")
+	elseif (curedCount >= 5 and QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)) then
+		return convoTemplate:getScreen("intro_completed_first_set")
+	elseif ((QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03) and curedCount < 15) or
+		(QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) and curedCount < 10) or
+		(QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01) and curedCount < 5)) then
+		return convoTemplate:getScreen("intro_in_progress")
+	elseif (not CreatureObject(pPlayer):hasSkill("science_medic_master")) then
+		convoTemplate:getScreen("intro_not_master_medic")
+	elseif (not QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03) and
+		QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)) then
+		return convoTemplate:getScreen("intro_start_third_set")
+	elseif (not QuestManager.hasActiveQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02) and
+		QuestManager.hasCompletedQuest(pPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)) then
+		return convoTemplate:getScreen("intro_start_second_set")
+	end
+
+	return convoTemplate:getScreen("intro")
+end
+
+function villageSivarraPhase1ConvoHandler:runScreenHandlers(conversationTemplate, conversingPlayer, conversingNPC, selectedOption, conversationScreen)
+	local screen = LuaConversationScreen(conversationScreen)
+	local screenID = screen:getScreenID()
+	local conversationScreen = screen:cloneScreen()
+	local clonedConversation = LuaConversationScreen(conversationScreen)
+
+	if (screenID == "great_explain") then
+		QuestManager.setCurrentQuestID(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)
+		QuestManager.activateQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)
+		FsMedicPuzzle:setCuredVillagerCount(conversingPlayer, 0)
+
+		ObjectManager.withCreaturePlayerObject(conversingPlayer, function(playerObject)
+			playerObject:addRewardedSchematic("object/draft_schematic/item/quest_item/fs_medic_puzzle_heal_pack.iff", 2, -1, true)
+		end)
+	elseif (screenID == "intro_completed_first_set") then
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_01)
+		VillageJediManagerCommon.unlockBranch(conversingPlayer, "force_sensitive_heightened_senses_persuasion")
+	elseif (screenID == "intro_completed_second_set") then
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)
+
+		local pInventory = SceneObject(conversingPlayer):getSlottedObject("inventory")
+
+		if (pInventory ~= nil) then
+			local pPendant = giveItem(pInventory, "object/tangible/wearables/necklace/necklace_ice_pendant.iff", -1, true)
+
+			if (pPendant == nil) then
+				CreatureObject(conversingPlayer):sendSystemMessage("Error: Unable to generate item.")
+			end
+		end
+	elseif (screenID == "intro_completed_third_set") then
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_03)
+		QuestManager.completeQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_FINISH)
+
+		local pInventory = SceneObject(conversingPlayer):getSlottedObject("inventory")
+
+		if (pInventory ~= nil) then
+			local pPlant = giveItem(pInventory, "object/tangible/loot/plant_grow/plant_stage_1.iff", -1, true)
+
+			if (pPlant == nil) then
+				CreatureObject(conversingPlayer):sendSystemMessage("Error: Unable to generate item.")
+			end
+		end
+
+		FsMedicPuzzle:cleanUp(conversingPlayer)
+	elseif (screenID == "talk_droids_again") then
+		QuestManager.activateQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)
+	elseif (screenID == "talk_droids_round_three") then
+		QuestManager.activateQuest(conversingPlayer, QuestManager.quests.FS_MEDIC_PUZZLE_QUEST_02)
+	elseif (screenID == "intro_in_progress") then
+		local count = tostring(FsMedicPuzzle:getPatientsLeftToTreat(conversingPlayer))
+		clonedConversation:setDialogTextTO(count)
+	end
+
+	return conversationScreen
+end
+
+function villageSivarraPhase1ConvoHandler:getNextConversationScreen(pConversationTemplate, pPlayer, selectedOption, pConversingNpc)
+	local pConversationSession = CreatureObject(pPlayer):getConversationSession()
+	local pLastConversationScreen = nil
+	if (pConversationSession ~= nil) then
+		local conversationSession = LuaConversationSession(pConversationSession)
+		pLastConversationScreen = conversationSession:getLastConversationScreen()
+	end
+	local conversationTemplate = LuaConversationTemplate(pConversationTemplate)
+	if (pLastConversationScreen ~= nil) then
+		local lastConversationScreen = LuaConversationScreen(pLastConversationScreen)
+		local optionLink = lastConversationScreen:getOptionLink(selectedOption)
+		return conversationTemplate:getScreen(optionLink)
+	end
+	return self:getInitialScreen(pPlayer, pConversingNpc, pConversationTemplate)
+end