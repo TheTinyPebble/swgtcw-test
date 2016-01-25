swgemu/Core3

[fixed] missing stats & effects on Chef food 'dustcrepe' (Mantis #6832)
Change-Id: I92faf93e939329ad335ca8b1b141eb217d53ed4c

MMOCoreORB/src/server/zone/objects/creature/damageovertime/DamageOverTime.h
@@ -54,6 +54,7 @@ public:
 	uint32 initDot(CreatureObject* victim, CreatureObject* attacker);
 	float reduceTick(float reduction);
 	void expireTick() { expires.updateToCurrentTime(); }
+	void reduceDuration (float multiplier);
 
 	// damage methods
 	inline uint32 doBleedingTick(CreatureObject* victim, CreatureObject* attacker);

MMOCoreORB/src/server/zone/objects/creature/damageovertime/DamageOverTimeList.h
@@ -42,6 +42,7 @@ public:
 	uint8 getRandomPool(uint64 dotType);
 	bool healState(CreatureObject* victim, uint64 dotType, float reduction);
 	void clear(CreatureObject* creature);
+	void reduceAllDOTDurations(float multiplier);
 	bool hasDot(uint64 dotType);
 
 	void sendStartMessage(CreatureObject* victim, uint64 type);
@@ -82,6 +83,5 @@ public:
 	}
 
 };
-
 
 #endif /* DAMAGEOVERTIMELIST_H_ */

TheTinyPebble/swgtcw-test

MMOCoreORB/src/server/zone/objects/creature/damageovertime/DamageOverTime.h
@@ -54,6 +54,7 @@ public:
 	uint32 initDot(CreatureObject* victim, CreatureObject* attacker);
 	float reduceTick(float reduction);
 	void expireTick() { expires.updateToCurrentTime(); }
+	void multiplyDuration (float multiplier);
 
 	// damage methods
 	inline uint32 doBleedingTick(CreatureObject* victim, CreatureObject* attacker);

MMOCoreORB/src/server/zone/objects/creature/damageovertime/DamageOverTimeList.h
@@ -43,6 +43,7 @@ public:
 	bool healState(CreatureObject* victim, uint64 dotType, float reduction);
 	void clear(CreatureObject* creature);
 	bool hasDot(uint64 dotType);
+	void multiplyAllDOTDurations (float multiplier);
 
 	void sendStartMessage(CreatureObject* victim, uint64 type);
 	void sendStopMessage(CreatureObject* victim, uint64 type);