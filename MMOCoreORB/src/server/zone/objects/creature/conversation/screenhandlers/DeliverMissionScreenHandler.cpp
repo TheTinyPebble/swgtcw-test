swgemu/Core3

[refactor] Changed static final int's to enum's in MissionObject to shut clang up
Change-Id: I918a34a76654d90baac6ca01d4f57df5be70252b

MMOCoreORB/src/server/zone/objects/creature/conversation/screenhandlers/DeliverMissionScreenHandler.cpp
@@ -25,8 +25,8 @@ MissionObject* DeliverMissionScreenHandler::getRelevantMissionObject(CreatureObj
 		if (datapad->getContainerObject(i)->isMissionObject()) {
 			Reference<MissionObject*> mission = datapad->getContainerObject(i).castTo<MissionObject*>();
 
-			if (mission != NULL && (mission->getTypeCRC() == MissionObject::DELIVER ||
-					mission->getTypeCRC() == MissionObject::CRAFTING)) {
+			if (mission != NULL && (mission->getTypeCRC() == MissionTypes::DELIVER ||
+					mission->getTypeCRC() == MissionTypes::CRAFTING)) {
 				DeliverMissionObjective* objective = cast<DeliverMissionObjective*>(mission->getMissionObjective());
 				if (objective != NULL) {
 					//Check if it is target or destination NPC
@@ -61,12 +61,12 @@ bool DeliverMissionScreenHandler::isSameSpawnPoint(const float& positionX, const
 }
 
 void DeliverMissionScreenHandler::performPickupConversation(ConversationScreen* conversationScreen, MissionObject* mission) {
-	if (mission->getTypeCRC() == MissionObject::DELIVER) {
+	if (mission->getTypeCRC() == MissionTypes::DELIVER) {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case Faction::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_deliver_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case Faction::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_deliver_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
 		default:
@@ -75,10 +75,10 @@ void DeliverMissionScreenHandler::performPickupConversation(ConversationScreen* 
 		}
 	} else {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case Faction::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case Faction::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
 		default:
@@ -89,12 +89,12 @@ void DeliverMissionScreenHandler::performPickupConversation(ConversationScreen* 
 }
 
 void DeliverMissionScreenHandler::performDeliverConversation(ConversationScreen* conversationScreen, MissionObject* mission) {
-	if (mission->getTypeCRC() == MissionObject::DELIVER) {
+	if (mission->getTypeCRC() == MissionTypes::DELIVER) {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case Faction::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_deliver_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case Faction::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_deliver_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
 		default:
@@ -103,10 +103,10 @@ void DeliverMissionScreenHandler::performDeliverConversation(ConversationScreen*
 		}
 	} else {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case Faction::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case Faction::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
 		default:

MMOCoreORB/src/server/zone/objects/mission/DeliverMissionObjectiveImplementation.cpp
@@ -91,10 +91,10 @@ bool DeliverMissionObjectiveImplementation::activateWithResult() {
 	//Select spawn type.
 	int spawnType = NpcSpawnPoint::NEUTRALSPAWN;
 	switch (mission->getFaction()) {
-	case MissionObject::FACTIONIMPERIAL:
+	case Faction::FACTIONIMPERIAL:
 		spawnType = NpcSpawnPoint::IMPERIALSPAWN;
 		break;
-	case MissionObject::FACTIONREBEL:
+	case Faction::FACTIONREBEL:
 		spawnType = NpcSpawnPoint::REBELSPAWN;
 		break;
 	default:

MMOCoreORB/src/server/zone/objects/mission/MissionObjectiveImplementation.cpp
@@ -111,7 +111,7 @@ void MissionObjectiveImplementation::awardFactionPoints() {
 	int factionPointsRebel = mission->getRewardFactionPointsRebel();
 	int factionPointsImperial = mission->getRewardFactionPointsImperial();
 
-	if ((factionPointsRebel <= 0 && factionPointsImperial <= 0) || mission->getFaction() == MissionObject::FACTIONNEUTRAL) {
+	if ((factionPointsRebel <= 0 && factionPointsImperial <= 0) || mission->getFaction() == Faction::FACTIONNEUTRAL) {
 		return;
 	}
 
@@ -125,7 +125,7 @@ void MissionObjectiveImplementation::awardFactionPoints() {
 
 			//Switch to get the correct order.
 			switch (mission->getFaction()) {
-			case MissionObject::FACTIONIMPERIAL:
+			case Faction::FACTIONIMPERIAL:
 				if (factionPointsImperial > 0) {
 					ghost->increaseFactionStanding("imperial", factionPointsImperial);
 				}
@@ -133,7 +133,7 @@ void MissionObjectiveImplementation::awardFactionPoints() {
 					ghost->decreaseFactionStanding("rebel", -factionPointsRebel);
 				}
 				break;
-			case MissionObject::FACTIONREBEL:
+			case Faction::FACTIONREBEL:
 				if (factionPointsRebel > 0) {
 					ghost->increaseFactionStanding("rebel", factionPointsRebel);
 				}

TheTinyPebble/swgtcw-test

MMOCoreORB/src/server/zone/objects/creature/conversation/screenhandlers/DeliverMissionScreenHandler.cpp
@@ -25,8 +25,8 @@ MissionObject* DeliverMissionScreenHandler::getRelevantMissionObject(CreatureObj
 		if (datapad->getContainerObject(i)->isMissionObject()) {
 			Reference<MissionObject*> mission = datapad->getContainerObject(i).castTo<MissionObject*>();
 
-			if (mission != NULL && (mission->getTypeCRC() == MissionObject::DELIVER ||
-					mission->getTypeCRC() == MissionObject::CRAFTING)) {
+			if (mission != NULL && (mission->getTypeCRC() == MissionTypes::DELIVER ||
+					mission->getTypeCRC() == MissionTypes::CRAFTING)) {
 				DeliverMissionObjective* objective = cast<DeliverMissionObjective*>(mission->getMissionObjective());
 				if (objective != NULL) {
 					//Check if it is target or destination NPC
@@ -61,12 +61,12 @@ bool DeliverMissionScreenHandler::isSameSpawnPoint(const float& positionX, const
 }
 
 void DeliverMissionScreenHandler::performPickupConversation(ConversationScreen* conversationScreen, MissionObject* mission) {
-	if (mission->getTypeCRC() == MissionObject::DELIVER) {
+	if (mission->getTypeCRC() == MissionTypes::DELIVER) {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case FactionManager::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_deliver_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case FactionManager::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_deliver_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
 		default:
@@ -75,10 +75,10 @@ void DeliverMissionScreenHandler::performPickupConversation(ConversationScreen* 
 		}
 	} else {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case FactionManager::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case FactionManager::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "p");
 			break;
 		default:
@@ -89,12 +89,12 @@ void DeliverMissionScreenHandler::performPickupConversation(ConversationScreen* 
 }
 
 void DeliverMissionScreenHandler::performDeliverConversation(ConversationScreen* conversationScreen, MissionObject* mission) {
-	if (mission->getTypeCRC() == MissionObject::DELIVER) {
+	if (mission->getTypeCRC() == MissionTypes::DELIVER) {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case FactionManager::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_deliver_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case FactionManager::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_deliver_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
 		default:
@@ -103,10 +103,10 @@ void DeliverMissionScreenHandler::performDeliverConversation(ConversationScreen*
 		}
 	} else {
 		switch (mission->getFaction()) {
-		case MissionObject::FACTIONIMPERIAL:
+		case FactionManager::FACTIONIMPERIAL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_imperial_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
-		case MissionObject::FACTIONREBEL:
+		case FactionManager::FACTIONREBEL:
 			conversationScreen->setDialogText("@mission/mission_npc_crafting_rebel_easy:m" + String::valueOf(mission->getMissionNumber()) + "r");
 			break;
 		default:

MMOCoreORB/src/server/zone/objects/mission/DeliverMissionObjectiveImplementation.cpp
@@ -91,10 +91,10 @@ bool DeliverMissionObjectiveImplementation::activateWithResult() {
 	//Select spawn type.
 	int spawnType = NpcSpawnPoint::NEUTRALSPAWN;
 	switch (mission->getFaction()) {
-	case MissionObject::FACTIONIMPERIAL:
+	case FactionManager::FACTIONIMPERIAL:
 		spawnType = NpcSpawnPoint::IMPERIALSPAWN;
 		break;
-	case MissionObject::FACTIONREBEL:
+	case FactionManager::FACTIONREBEL:
 		spawnType = NpcSpawnPoint::REBELSPAWN;
 		break;
 	default:

MMOCoreORB/src/server/zone/objects/mission/MissionObjectiveImplementation.cpp
@@ -111,7 +111,7 @@ void MissionObjectiveImplementation::awardFactionPoints() {
 	int factionPointsRebel = mission->getRewardFactionPointsRebel();
 	int factionPointsImperial = mission->getRewardFactionPointsImperial();
 
-	if ((factionPointsRebel <= 0 && factionPointsImperial <= 0) || mission->getFaction() == MissionObject::FACTIONNEUTRAL) {
+	if ((factionPointsRebel <= 0 && factionPointsImperial <= 0) || mission->getFaction() == FactionManager::FACTIONNEUTRAL) {
 		return;
 	}
 
@@ -125,7 +125,7 @@ void MissionObjectiveImplementation::awardFactionPoints() {
 
 			//Switch to get the correct order.
 			switch (mission->getFaction()) {
-			case MissionObject::FACTIONIMPERIAL:
+			case FactionManager::FACTIONIMPERIAL:
 				if (factionPointsImperial > 0) {
 					ghost->increaseFactionStanding("imperial", factionPointsImperial);
 				}
@@ -133,7 +133,7 @@ void MissionObjectiveImplementation::awardFactionPoints() {
 					ghost->decreaseFactionStanding("rebel", -factionPointsRebel);
 				}
 				break;
-			case MissionObject::FACTIONREBEL:
+			case FactionManager::FACTIONREBEL:
 				if (factionPointsRebel > 0) {
 					ghost->increaseFactionStanding("rebel", factionPointsRebel);
 				}