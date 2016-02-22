swgemu/Core3

[Added] Chat room op, deop, ban and unban commands. [Added] System message notifications when player is kicked from a chat room. [Fixed] Uninviting a player also kicks them from the private room. [Fixed] Only moderators can chat in a "moderated" room (all rooms can have moderators). [Fixed] Moderators can now use their chat commands while the target is offline. [Fixed] Cases where the chat room player list doesn't update properly. [Fixed] Proper error message is displayed when attempting to join a room you're banned from. [Note] The deop result messages are not working correctly. This might be a client-side bug.
Change-Id: Ib9d7de7457295beda2229c20a94b6b61094a98e2

MMOCoreORB/src/server/zone/packets/chat/ChatBanFromRoomCallback.h
@@ -1,0 +1,58 @@
+/*
+ * ChatBanFromRoomCallback.h
+ *
+ *  Created on: Feb 18, 2016
+ *      Author: Anakis
+ */
+
+#ifndef CHATBANFROMROOMCALLBACK_H_
+#define CHATBANFROMROOMCALLBACK_H_
+
+
+#include "server/zone/packets/MessageCallback.h"
+#include "server/chat/ChatManager.h"
+
+class ChatBanFromRoomCallback : public MessageCallback {
+	String baneeName; //Player to invite.
+	String roomPath; //Room to invite player to.
+	int requestID;
+
+public:
+	ChatBanFromRoomCallback(ZoneClientSession* client, ZoneProcessServer* server) : MessageCallback(client, server) {
+		baneeName = "";
+		roomPath = "";
+		requestID = 0;
+	}
+
+	void parse(Message* message) {
+		String unused;
+
+		message->parseAscii(unused); //Game name
+		message->parseAscii(unused); //Galaxy name
+		message->parseAscii(baneeName); //Player invited
+		message->parseAscii(roomPath); //Path to room
+		requestID = message->parseInt(); //Request ID
+
+	}
+
+	void run() {
+		ManagedReference<SceneObject*> scene = client->getPlayer();
+
+		if (scene == NULL)
+			return;
+
+		CreatureObject* banner = cast<CreatureObject*>(scene.get());
+
+		if (banner == NULL)
+			return;
+
+		ManagedReference<ChatManager*> chatManager = server->getChatManager();
+		if (chatManager != NULL)
+			chatManager->handleChatBanPlayer(banner, baneeName, roomPath, requestID);
+
+	}
+
+};
+
+
+#endif /* CHATBANFROMROOMCALLBACK_H_ */

MMOCoreORB/src/server/zone/packets/chat/ChatOnBanFromRoom.h
@@ -1,0 +1,38 @@
+/*
+ * ChatOnBanFromRoom.h
+ *
+ *  Created on: Feb 19, 2016
+ *      Author: Anakis
+ */
+
+#ifndef CHATONBANFROMROOM_H_
+#define CHATONBANFROMROOM_H_
+
+#include "engine/engine.h"
+
+class ChatOnBanFromRoom : public BaseMessage {
+public:
+	ChatOnBanFromRoom(CreatureObject* banner, const String& baneeName, const String& roomPath, int error, int requestID = 0) : BaseMessage() {
+		String galaxy = banner->getZoneServer()->getGalaxyName();
+
+		insertShort(0x06);
+		insertInt(0x5A38538D);  //Opcode
+
+		insertAscii(roomPath); //Full room path
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); //Galaxy name
+		insertAscii(banner->getFirstName()); //Inviter's first name
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); // Galaxy name
+		insertAscii(baneeName); //Banee's first name
+
+		insertInt(error); ///Result code
+		insertInt(requestID); //Request ID
+
+	}
+
+};
+
+#endif /*CHATONBANFROMROOM_H_*/

MMOCoreORB/src/server/zone/packets/chat/ChatOnUnbanFromRoom.h
@@ -1,0 +1,38 @@
+/*
+ * ChatOnUnbanFromRoom.h
+ *
+ *  Created on: Feb 19, 2016
+ *      Author: Anakis
+ */
+
+#ifndef CHATONUNBANFROMROOM_H_
+#define CHATONUNBANFROMROOM_H_
+
+#include "engine/engine.h"
+
+class ChatOnUnbanFromRoom : public BaseMessage {
+public:
+	ChatOnUnbanFromRoom(CreatureObject* unbanner, const String& unbaneeName, const String& roomPath, int error, int requestID = 0) : BaseMessage() {
+		String galaxy = unbanner->getZoneServer()->getGalaxyName();
+
+		insertShort(0x06);
+		insertInt(0xBAF9B815);  //Opcode
+
+		insertAscii(roomPath); //Full room path
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); //Galaxy name
+		insertAscii(unbanner->getFirstName()); //Inviter's first name
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); // Galaxy name
+		insertAscii(unbaneeName); //Banee's first name
+
+		insertInt(error); ///Result code
+		insertInt(requestID); //Request ID
+
+	}
+
+};
+
+#endif /*CHATONUNBANFROMROOM_H_*/

TheTinyPebble/swgtcw-test

MMOCoreORB/src/server/zone/packets/chat/ChatBanFromRoomCallback.h
@@ -1,0 +1,58 @@
+/*
+ * ChatBanFromRoomCallback.h
+ *
+ *  Created on: Feb 18, 2016
+ *      Author: Anakis
+ */
+
+#ifndef CHATBANFROMROOMCALLBACK_H_
+#define CHATBANFROMROOMCALLBACK_H_
+
+
+#include "server/zone/packets/MessageCallback.h"
+#include "server/chat/ChatManager.h"
+
+class ChatBanFromRoomCallback : public MessageCallback {
+	String baneeName; //Player to ban.
+	String roomPath; //Room to ban the player from.
+	int requestID;
+
+public:
+	ChatBanFromRoomCallback(ZoneClientSession* client, ZoneProcessServer* server) : MessageCallback(client, server) {
+		baneeName = "";
+		roomPath = "";
+		requestID = 0;
+	}
+
+	void parse(Message* message) {
+		String unused;
+
+		message->parseAscii(unused); //Game name
+		message->parseAscii(unused); //Galaxy name
+		message->parseAscii(baneeName); //Player to ban
+		message->parseAscii(roomPath); //Path to room
+		requestID = message->parseInt(); //Request ID
+
+	}
+
+	void run() {
+		ManagedReference<SceneObject*> scene = client->getPlayer();
+
+		if (scene == NULL)
+			return;
+
+		CreatureObject* banner = cast<CreatureObject*>(scene.get());
+
+		if (banner == NULL)
+			return;
+
+		ManagedReference<ChatManager*> chatManager = server->getChatManager();
+		if (chatManager != NULL)
+			chatManager->handleChatBanPlayer(banner, baneeName, roomPath, requestID);
+
+	}
+
+};
+
+
+#endif /* CHATBANFROMROOMCALLBACK_H_ */

MMOCoreORB/src/server/zone/packets/chat/ChatOnBanFromRoom.h
@@ -1,0 +1,38 @@
+/*
+ * ChatOnBanFromRoom.h
+ *
+ *  Created on: Feb 19, 2016
+ *      Author: Anakis
+ */
+
+#ifndef CHATONBANFROMROOM_H_
+#define CHATONBANFROMROOM_H_
+
+#include "engine/engine.h"
+
+class ChatOnBanFromRoom : public BaseMessage {
+public:
+	ChatOnBanFromRoom(CreatureObject* banner, const String& baneeName, const String& roomPath, int error, int requestID = 0) : BaseMessage() {
+		String galaxy = banner->getZoneServer()->getGalaxyName();
+
+		insertShort(0x06);
+		insertInt(0x5A38538D);  //Opcode
+
+		insertAscii(roomPath); //Full room path
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); //Galaxy name
+		insertAscii(banner->getFirstName()); //Banner's first name
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); // Galaxy name
+		insertAscii(baneeName); //Banee's first name
+
+		insertInt(error); ///Result code
+		insertInt(requestID); //Request ID
+
+	}
+
+};
+
+#endif /*CHATONBANFROMROOM_H_*/

MMOCoreORB/src/server/zone/packets/chat/ChatOnUnbanFromRoom.h
@@ -1,0 +1,38 @@
+/*
+ * ChatOnUnbanFromRoom.h
+ *
+ *  Created on: Feb 19, 2016
+ *      Author: Anakis
+ */
+
+#ifndef CHATONUNBANFROMROOM_H_
+#define CHATONUNBANFROMROOM_H_
+
+#include "engine/engine.h"
+
+class ChatOnUnbanFromRoom : public BaseMessage {
+public:
+	ChatOnUnbanFromRoom(CreatureObject* unbanner, const String& unbaneeName, const String& roomPath, int error, int requestID = 0) : BaseMessage() {
+		String galaxy = unbanner->getZoneServer()->getGalaxyName();
+
+		insertShort(0x06);
+		insertInt(0xBAF9B815);  //Opcode
+
+		insertAscii(roomPath); //Full room path
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); //Galaxy name
+		insertAscii(unbanner->getFirstName()); //Unbanner's first name
+
+		insertAscii("SWG"); //Game
+		insertAscii(galaxy); // Galaxy name
+		insertAscii(unbaneeName); //Unbanee's first name
+
+		insertInt(error); ///Result code
+		insertInt(requestID); //Request ID
+
+	}
+
+};
+
+#endif /*CHATONUNBANFROMROOM_H_*/