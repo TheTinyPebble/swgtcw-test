swgemu/Core3

[fixed] Hair object in inventory after image design session (#2067) [added] LambdaObserver and LambdaObserverFunction class to use with existing observer system [added] std=c++11 to Makefile.am - You will need a C++11 compiler [added] CREATUREREVIVED observer type and notify functions [fixed] Feign death removal on character load [refactor] Changed some prose packages to use DisplayedName instead of their objectID [refactor] Removed SCHEDULE_TASK macros and replaced them with TaskManager::scheduleTask lambdas
Change-Id: Ib25cdeabcda7bb78c9a792fed66d27753bca8234

MMOCoreORB/src/server/zone/objects/tangible/eventperk/ShuttleBeaconImplementation.cpp
@@ -12,7 +12,6 @@
 #include "server/zone/managers/planet/PlanetManager.h"
 #include "server/zone/managers/structure/StructureManager.h"
 #include "server/zone/objects/player/sui/callbacks/ShuttleBeaconSuiCallback.h"
-#include "engine/task/ScheduledTask.h"
 
 void ShuttleBeaconImplementation::initializeTransientMembers() {
 	TangibleObjectImplementation::initializeTransientMembers();
@@ -63,12 +62,12 @@ int ShuttleBeaconImplementation::handleObjectMenuSelect(CreatureObject* player, 
 
 		ManagedReference<ShuttleBeacon*> tempBeacon = _this.getReferenceUnsafeStaticCast();
 
-		SCHEDULE_TASK_2(tempBeacon, player, 250, {
-			if (tempBeacon_p != NULL) {
-				Locker locker(tempBeacon_p);
-				tempBeacon_p->dismissShuttle(player_p);
+		Core::getTaskManager()->scheduleTask([tempBeacon, player]{
+			if(tempBeacon != NULL) {
+				Locker locker(tempBeacon);
+				tempBeacon->dismissShuttle(player);
 			}
-		});
+		}, "DismissShuttleTask", 250);
 	}
 
 	return 0;
@@ -163,12 +162,10 @@ void ShuttleBeaconImplementation::spawnShuttle(CreatureObject* player, int type)
 	if (tempBeacon == NULL)
 		return;
 
-	SCHEDULE_TASK_2(tempBeacon, player, 250, {
-		if (tempBeacon_p != NULL) {
-			Locker locker(tempBeacon_p);
-			tempBeacon_p->landShuttle(player_p);
-		}
-	});
+	Core::getTaskManager()->scheduleTask([tempBeacon, player] {
+		Locker locker(tempBeacon);
+		tempBeacon->landShuttle(player);
+	}, "LandShuttleTask", 250);
 }
 
 void ShuttleBeaconImplementation::landShuttle(CreatureObject* player) {
@@ -203,12 +200,11 @@ void ShuttleBeaconImplementation::landShuttle(CreatureObject* player) {
 
 	shuttleStatus = 1;
 
-	SCHEDULE_TASK_1(tempBeacon, 40000, {
-		if (tempBeacon_p != NULL) {
-			Locker locker(tempBeacon_p);
-			tempBeacon_p->setReadyToTakeOff(true);
-		}
-	});
+
+	Core::getTaskManager()->scheduleTask([tempBeacon] {
+		Locker locker(tempBeacon);
+		tempBeacon->setReadyToTakeOff(true);
+	}, "ShuttleTakeOffTask", 40000);
 }
 
 void ShuttleBeaconImplementation::dismissShuttle(CreatureObject* player) {
@@ -241,12 +237,11 @@ void ShuttleBeaconImplementation::dismissShuttle(CreatureObject* player) {
 	shuttleStatus = 0;
 	player->sendSystemMessage("@event_perk:shuttle_is_leaving"); // Transmission Recieved: Shuttle is leaving the area.
 
-	SCHEDULE_TASK_2(tempBeacon, player, 20000, {
-		if (tempBeacon_p != NULL) {
-			Locker locker(tempBeacon_p);
-			tempBeacon_p->destroyShuttle(player_p);
-		}
-	});
+
+	Core::getTaskManager()->scheduleTask([tempBeacon, player] {
+		Locker locker(tempBeacon);
+		tempBeacon->destroyShuttle(player);
+	}, "DestroyShuttleTask", 20000);
 }
 
 void ShuttleBeaconImplementation::destroyShuttle(CreatureObject* player) {

TheTinyPebble/swgtcw-test

MMOCoreORB/src/server/zone/objects/tangible/eventperk/ShuttleBeaconImplementation.cpp
@@ -12,7 +12,6 @@
 #include "server/zone/managers/planet/PlanetManager.h"
 #include "server/zone/managers/structure/StructureManager.h"
 #include "server/zone/objects/player/sui/callbacks/ShuttleBeaconSuiCallback.h"
-#include "engine/task/ScheduledTask.h"
 
 void ShuttleBeaconImplementation::initializeTransientMembers() {
 	TangibleObjectImplementation::initializeTransientMembers();
@@ -62,13 +61,13 @@ int ShuttleBeaconImplementation::handleObjectMenuSelect(CreatureObject* player, 
 		}
 
 		ManagedReference<ShuttleBeacon*> tempBeacon = _this.getReferenceUnsafeStaticCast();
-
-		SCHEDULE_TASK_2(tempBeacon, player, 250, {
-			if (tempBeacon_p != NULL) {
-				Locker locker(tempBeacon_p);
-				tempBeacon_p->dismissShuttle(player_p);
+		Reference<CreatureObject*> creo = player;
+		Core::getTaskManager()->scheduleTask([tempBeacon, creo]{
+			if(tempBeacon != NULL) {
+				Locker locker(tempBeacon);
+				tempBeacon->dismissShuttle(creo);
 			}
-		});
+		}, "DismissShuttleTask", 250);
 	}
 
 	return 0;
@@ -163,12 +162,11 @@ void ShuttleBeaconImplementation::spawnShuttle(CreatureObject* player, int type)
 	if (tempBeacon == NULL)
 		return;
 
-	SCHEDULE_TASK_2(tempBeacon, player, 250, {
-		if (tempBeacon_p != NULL) {
-			Locker locker(tempBeacon_p);
-			tempBeacon_p->landShuttle(player_p);
-		}
-	});
+	Reference<CreatureObject*> creo = player;
+	Core::getTaskManager()->scheduleTask([tempBeacon, creo] {
+		Locker locker(tempBeacon);
+		tempBeacon->landShuttle(creo);
+	}, "LandShuttleTask", 250);
 }
 
 void ShuttleBeaconImplementation::landShuttle(CreatureObject* player) {
@@ -203,12 +201,11 @@ void ShuttleBeaconImplementation::landShuttle(CreatureObject* player) {
 
 	shuttleStatus = 1;
 
-	SCHEDULE_TASK_1(tempBeacon, 40000, {
-		if (tempBeacon_p != NULL) {
-			Locker locker(tempBeacon_p);
-			tempBeacon_p->setReadyToTakeOff(true);
-		}
-	});
+
+	Core::getTaskManager()->scheduleTask([tempBeacon] {
+		Locker locker(tempBeacon);
+		tempBeacon->setReadyToTakeOff(true);
+	}, "ShuttleTakeOffTask", 40000);
 }
 
 void ShuttleBeaconImplementation::dismissShuttle(CreatureObject* player) {
@@ -241,12 +238,12 @@ void ShuttleBeaconImplementation::dismissShuttle(CreatureObject* player) {
 	shuttleStatus = 0;
 	player->sendSystemMessage("@event_perk:shuttle_is_leaving"); // Transmission Recieved: Shuttle is leaving the area.
 
-	SCHEDULE_TASK_2(tempBeacon, player, 20000, {
-		if (tempBeacon_p != NULL) {
-			Locker locker(tempBeacon_p);
-			tempBeacon_p->destroyShuttle(player_p);
-		}
-	});
+
+	Reference<CreatureObject*> creo = player;
+	Core::getTaskManager()->scheduleTask([tempBeacon, creo] {
+		Locker locker(tempBeacon);
+		tempBeacon->destroyShuttle(creo);
+	}, "DestroyShuttleTask", 20000);
 }
 
 void ShuttleBeaconImplementation::destroyShuttle(CreatureObject* player) {