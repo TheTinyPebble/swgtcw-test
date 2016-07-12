swgemu/Core3

[added] 'The Nightsister Themepark' to Dathomir (Mantis #6846)
Change-Id: I9e3a8eeb57390fff2972d72542e7ba22c58b3248

MMOCoreORB/bin/scripts/mobile/quest/dathomir/theme_park_nightsister_outcast.lua
@@ -1,0 +1,37 @@
+theme_park_nightsister_outcast = Creature:new {
+  objectName = "@mob/creature_names:nightsister_outcast",
+  randomNameType = NAME_GENERIC,
+  randomNameTag = true,
+  socialGroup = "nightsister",
+  faction = "nightsister",
+  level = 81,
+  chanceHit = 0.75,
+  damageMin = 555,
+  damageMax = 820,
+  baseXp = 7761,
+  baseHAM = 12000,
+  baseHAMmax = 15000,
+  armor = 1,
+  resists = {30,30,30,100,100,100,100,100,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = NONE,
+  creatureBitmask = NONE,
+  optionsBitmask = AIENABLED + CONVERSABLE + INTERESTING,
+  diet = HERBIVORE,
+
+  templates = {"object/mobile/dressed_dathomir_nightsister_outcast.iff"},
+  lootGroups = {},
+  weapons = {},
+  conversationTemplate = "theme_park_nightsister_mission_target_convotemplate",
+  attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_nightsister_outcast, "theme_park_nightsister_outcast")

MMOCoreORB/bin/scripts/mobile/quest/dathomir/theme_park_nightsister_rancor.lua
@@ -1,0 +1,36 @@
+theme_park_nightsister_rancor = Creature:new {
+  objectName = "@mob/creature_names:nightsister_rancor",
+  socialGroup = "nightsister",
+  faction = "nightsister",
+  level = 55,
+  chanceHit = 0.5,
+  damageMin = 395,
+  damageMax = 500,
+  baseXp = 5373,
+  baseHAM = 10000,
+  baseHAMmax = 12000,
+  armor = 1,
+  resists = {130,160,5,200,200,200,5,5,-1},
+  meatType = "meat_carnivore",
+  meatAmount = 950,
+  hideType = "hide_leathery",
+  hideAmount = 876,
+  boneType = "bone_mammal",
+  boneAmount = 776,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 10,
+  pvpBitmask = NONE,
+  creatureBitmask = NONE,
+  optionsBitmask = AIENABLED + CONVERSABLE,
+  diet = CARNIVORE,
+
+  templates = {"object/mobile/rancor.iff"},
+  scale = 1.05,
+  lootGroups = {},
+  weapons = {},
+  conversationTemplate = "theme_park_nightsister_mission_target_convotemplate",
+  attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_nightsister_rancor, "theme_park_nightsister_rancor")

TheTinyPebble/swgtcw-test

MMOCoreORB/bin/scripts/mobile/quest/dathomir/theme_park_nightsister_outcast.lua
@@ -1,0 +1,37 @@
+theme_park_nightsister_outcast = Creature:new {
+  objectName = "@mob/creature_names:nightsister_outcast",
+  randomNameType = NAME_GENERIC,
+  randomNameTag = true,
+  socialGroup = "nightsister",
+  faction = "nightsister",
+  level = 81,
+  chanceHit = 0.75,
+  damageMin = 555,
+  damageMax = 820,
+  baseXp = 7761,
+  baseHAM = 12000,
+  baseHAMmax = 15000,
+  armor = 1,
+  resists = {30,30,30,100,100,100,100,100,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = ATTACKABLE,
+  creatureBitmask = NONE,
+  optionsBitmask = AIENABLED + CONVERSABLE,
+  diet = HERBIVORE,
+
+  templates = {"object/mobile/dressed_dathomir_nightsister_outcast.iff"},
+  lootGroups = {},
+  weapons = {},
+  conversationTemplate = "theme_park_nightsister_mission_target_convotemplate",
+  attacks = merge(tkamid,brawlermaster)
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_nightsister_outcast, "theme_park_nightsister_outcast")

MMOCoreORB/bin/scripts/mobile/quest/dathomir/theme_park_nightsister_rancor.lua
@@ -1,0 +1,36 @@
+theme_park_nightsister_rancor = Creature:new {
+  objectName = "@mob/creature_names:nightsister_rancor",
+  socialGroup = "nightsister",
+  faction = "nightsister",
+  level = 55,
+  chanceHit = 0.5,
+  damageMin = 395,
+  damageMax = 500,
+  baseXp = 5373,
+  baseHAM = 10000,
+  baseHAMmax = 12000,
+  armor = 1,
+  resists = {130,160,5,200,200,200,5,5,-1},
+  meatType = "meat_carnivore",
+  meatAmount = 950,
+  hideType = "hide_leathery",
+  hideAmount = 876,
+  boneType = "bone_mammal",
+  boneAmount = 776,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 10,
+  pvpBitmask = ATTACKABLE,
+  creatureBitmask = NONE,
+  optionsBitmask = AIENABLED + CONVERSABLE,
+  diet = CARNIVORE,
+
+  templates = {"object/mobile/rancor.iff"},
+  scale = 1.05,
+  lootGroups = {},
+  weapons = {},
+  conversationTemplate = "theme_park_nightsister_mission_target_convotemplate",
+  attacks = {}
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_nightsister_rancor, "theme_park_nightsister_rancor")