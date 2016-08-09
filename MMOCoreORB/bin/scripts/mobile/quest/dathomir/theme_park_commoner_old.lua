swgemu/Core3

[added] the SMC themepark to Dathomir (Mantis #6900) -- PHASE 1 (Zideera portion)
Change-Id: Ibf46b734cf78d2679746963a056c96b092420a4b

MMOCoreORB/bin/scripts/mobile/quest/dathomir/theme_park_commoner_old.lua
@@ -1,0 +1,41 @@
+theme_park_commoner_old = Creature:new {
+  objectName = "@mob/creature_names:commoner",
+  randomNameType = NAME_GENERIC,
+  randomNameTag = true,
+  socialGroup = "townsperson",
+  faction = "townsperson",
+  level = 4,
+  chanceHit = 0.24,
+  damageMin = 40,
+  damageMax = 45,
+  baseXp = 62,
+  baseHAM = 113,
+  baseHAMmax = 138,
+  armor = 0,
+  resists = {15,15,15,15,15,15,15,-1,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = ATTACKABLE,
+  creatureBitmask = HERD,
+  optionsBitmask = AIENABLED + CONVERSABLE,
+  diet = HERBIVORE,
+
+  templates = { "object/mobile/dressed_criminal_pirate_human_male_01.iff",
+      "object/mobile/dressed_criminal_thug_human_male_01.iff",
+      "object/mobile/dressed_criminal_thug_zabrak_male_01.iff"
+      },
+
+  lootGroups = {},
+  weapons = {"pirate_weapons_light"},
+  conversationTemplate = "theme_park_singing_mountain_clan_zideera_mission_target_convotemplate",
+  attacks = merge(marksmannovice,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_commoner_old, "theme_park_commoner_old")

MMOCoreORB/bin/scripts/mobile/quest/dathomir/zideera.lua
@@ -23,13 +23,13 @@ zideera = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = AIENABLED,
+	optionsBitmask = AIENABLED + CONVERSABLE,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_zideera.iff"},
 	lootGroups = {},
 	weapons = {},
-	conversationTemplate = "",
+	conversationTemplate = "theme_park_singing_mountain_clan_zideera_mission_giver_convotemplate",
 	attacks = {
 	}
 }

MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -42,6 +42,7 @@ includeFile("themepark/jabba/themeParkJabba.lua")
 includeFile("themepark/valarian/themeParkValarian.lua")
 includeFile("themepark/marauder/themeParkMarauder.lua")
 includeFile("themepark/nightsister/themeParkNightsister.lua")
+includeFile("themepark/smc/themeParkSingingMountainClanZideera.lua")
 
 -- Staff tools
 includeFile("tools/tools.lua")

TheTinyPebble/swgtcw-test

MMOCoreORB/bin/scripts/mobile/quest/dathomir/theme_park_commoner_old.lua
@@ -1,0 +1,41 @@
+theme_park_commoner_old = Creature:new {
+  objectName = "@mob/creature_names:commoner",
+  randomNameType = NAME_GENERIC,
+  randomNameTag = true,
+  socialGroup = "townsperson",
+  faction = "townsperson",
+  level = 4,
+  chanceHit = 0.24,
+  damageMin = 40,
+  damageMax = 45,
+  baseXp = 62,
+  baseHAM = 113,
+  baseHAMmax = 138,
+  armor = 0,
+  resists = {15,15,15,15,15,15,15,-1,-1},
+  meatType = "",
+  meatAmount = 0,
+  hideType = "",
+  hideAmount = 0,
+  boneType = "",
+  boneAmount = 0,
+  milk = 0,
+  tamingChance = 0,
+  ferocity = 0,
+  pvpBitmask = ATTACKABLE,
+  creatureBitmask = HERD,
+  optionsBitmask = AIENABLED + CONVERSABLE,
+  diet = HERBIVORE,
+
+  templates = { "object/mobile/dressed_criminal_pirate_human_male_01.iff",
+      "object/mobile/dressed_criminal_thug_human_male_01.iff",
+      "object/mobile/dressed_criminal_thug_zabrak_male_01.iff"
+      },
+
+  lootGroups = {},
+  weapons = {"pirate_weapons_light"},
+  conversationTemplate = "theme_park_smc_zideera_mission_target_convotemplate",
+  attacks = merge(marksmannovice,brawlernovice)
+}
+
+CreatureTemplates:addCreatureTemplate(theme_park_commoner_old, "theme_park_commoner_old")

MMOCoreORB/bin/scripts/mobile/quest/dathomir/zideera.lua
@@ -23,13 +23,13 @@ zideera = Creature:new {
 	ferocity = 0,
 	pvpBitmask = NONE,
 	creatureBitmask = PACK,
-	optionsBitmask = AIENABLED,
+	optionsBitmask = AIENABLED + CONVERSABLE,
 	diet = HERBIVORE,
 
 	templates = {"object/mobile/dressed_zideera.iff"},
 	lootGroups = {},
 	weapons = {},
-	conversationTemplate = "",
+	conversationTemplate = "theme_park_smc_zideera_mission_giver_convotemplate",
 	attacks = {
 	}
 }

MMOCoreORB/bin/scripts/screenplays/screenplays.lua
@@ -42,6 +42,7 @@ includeFile("themepark/jabba/themeParkJabba.lua")
 includeFile("themepark/valarian/themeParkValarian.lua")
 includeFile("themepark/marauder/themeParkMarauder.lua")
 includeFile("themepark/nightsister/themeParkNightsister.lua")
+includeFile("themepark/smc/themeParkSmcZideera.lua")
 
 -- Staff tools
 includeFile("tools/tools.lua")