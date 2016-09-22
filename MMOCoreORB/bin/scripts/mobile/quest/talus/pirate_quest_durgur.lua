swgemu/Core3

[Added] the 'Durgur Pyne' missions to TALUS (Mantis #7172, phase 1) -- [fixed] a few 'pirate_ ' mobs to have correct names/templates
Change-Id: I7c4c0917d1b1fc7895fa34eb4980c846fdc5c23c

MMOCoreORB/bin/scripts/mobile/quest/talus/pirate_quest_durgur.lua
@@ -1,0 +1,48 @@
+pirate_quest_durgur = Creature:new {
+	objectName = "@mob/creature_names:pirate_lieutenant",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	socialGroup = "pirate",
+	faction = "pirate",
+	level = 15,
+	chanceHit = 0.31,
+	damageMin = 160,
+	damageMax = 170,
+	baseXp = 960,
+	baseHAM = 2400,
+	baseHAMmax = 3000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK,
+	optionsBitmask = AIENABLED + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {
+		"object/mobile/dressed_criminal_pirate_human_male_01.iff",
+		"object/mobile/dressed_criminal_thug_aqualish_male_01.iff",
+		"object/mobile/dressed_criminal_thug_aqualish_male_02.iff",
+		"object/mobile/dressed_criminal_thug_bothan_male_01.iff",
+		"object/mobile/dressed_criminal_thug_human_male_01.iff",
+		"object/mobile/dressed_criminal_thug_human_male_02.iff",
+		"object/mobile/dressed_criminal_thug_rodian_male_01.iff",
+		"object/mobile/dressed_criminal_thug_trandoshan_male_01.iff",
+		"object/mobile/dressed_criminal_thug_zabrak_male_01.iff"
+	},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "durgur_pyne_mission_target_convotemplate",
+	reactionStf = "@npc_reaction/slang",
+	attacks = merge(brawlermaster, teraskasinovice)
+}
+
+CreatureTemplates:addCreatureTemplate(pirate_quest_durgur, "pirate_quest_durgur")

MMOCoreORB/bin/scripts/screenplays/tasks/talus/durgur_pyne.lua
@@ -1,0 +1,91 @@
+durgur_pyne_missions =
+	{
+		{
+			missionType = "escort",
+			primarySpawns =
+			{
+				{ npcTemplate = "pirate_quest_durgur", npcName = "a Pirate Lieutenant" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "pirate_armsman", npcName = "" },
+				{ npcTemplate = "pirate_armsman", npcName = "" }
+			},
+			itemSpawns = {},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 25 },
+			}
+		},
+		{
+			missionType = "escort",
+			primarySpawns =
+			{
+				{ npcTemplate = "agent_quest_durgur", npcName = "" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "pirate_cutthroat", npcName = "" },
+				{ npcTemplate = "pirate_armsman", npcName = "" },
+				{ npcTemplate = "pirate_armsman", npcName = "" }
+			},
+			itemSpawns = 
+			{
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 50 },
+			}
+		},
+		{
+			missionType = "assassinate",
+			primarySpawns =
+			{
+				{ npcTemplate = "pirate_captain", npcName = "a Pirate Captain" }
+			},
+			secondarySpawns =
+			{--*questgiver Durgur says in string-file dialog "I'll contact a couple of my officers to give you some backup"
+				--but i don't see how we can spawn FRIENDLY npcs with our current tp logic for 'Secondarys'?
+					--Maybe add a new mission option called 'friendlySpawns' ??
+				{ npcTemplate = "pirate_privateer", npcName = "" },
+				{ npcTemplate = "pirate_crewman", npcName = "" },
+				{ npcTemplate = "pirate_crewman", npcName = "" },
+				{ npcTemplate = "pirate_crewman", npcName = "" }
+			},
+			itemSpawns = 
+			{
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 75 },
+			}
+		}
+	}
+
+npcMapDurgurPyne =
+	{
+		{
+			spawnData = { npcTemplate = "durgur_pyne", x = 3.0, z = -0.1, y = 4.4, direction = -22, cellID = 4265614, position = STAND },
+			worldPosition = { x = 4546, y = 5149 },
+			npcNumber = 1,
+			stfFile = "@static_npc/talus/talus_nashal_durgur_pyne",
+			missions = durgur_pyne_missions
+		},
+	}
+
+DurgurPyne = ThemeParkLogic:new {
+	npcMap = npcMapDurgurPyne,
+	className = "DurgurPyne",
+	screenPlayState = "durgur_pyne_tasks",
+	planetName = "talus",
+	distance = 900
+}
+
+registerScreenPlay("DurgurPyne", true)
+
+durgur_pyne_mission_giver_conv_handler = mission_giver_conv_handler:new {
+	themePark = DurgurPyne
+}
+durgur_pyne_mission_target_conv_handler = mission_target_conv_handler:new {
+	themePark = DurgurPyne
+}

TheTinyPebble/swgtcw-test

MMOCoreORB/bin/scripts/mobile/quest/talus/pirate_quest_durgur.lua
@@ -1,0 +1,48 @@
+pirate_quest_durgur = Creature:new {
+	objectName = "@mob/creature_names:sleemo_scamp",
+	randomNameType = NAME_GENERIC,
+	randomNameTag = true,
+	socialGroup = "sleemo_gang",
+	faction = "",
+	level = 15,
+	chanceHit = 0.31,
+	damageMin = 160,
+	damageMax = 170,
+	baseXp = 960,
+	baseHAM = 2400,
+	baseHAMmax = 3000,
+	armor = 0,
+	resists = {0,0,0,0,0,0,0,-1,-1},
+	meatType = "",
+	meatAmount = 0,
+	hideType = "",
+	hideAmount = 0,
+	boneType = "",
+	boneAmount = 0,
+	milk = 0,
+	tamingChance = 0,
+	ferocity = 0,
+	pvpBitmask = ATTACKABLE,
+	creatureBitmask = PACK,
+	optionsBitmask = AIENABLED + CONVERSABLE,
+	diet = HERBIVORE,
+
+	templates = {
+		"object/mobile/dressed_criminal_pirate_human_male_01.iff",
+		"object/mobile/dressed_criminal_thug_aqualish_male_01.iff",
+		"object/mobile/dressed_criminal_thug_aqualish_male_02.iff",
+		"object/mobile/dressed_criminal_thug_bothan_male_01.iff",
+		"object/mobile/dressed_criminal_thug_human_male_01.iff",
+		"object/mobile/dressed_criminal_thug_human_male_02.iff",
+		"object/mobile/dressed_criminal_thug_rodian_male_01.iff",
+		"object/mobile/dressed_criminal_thug_trandoshan_male_01.iff",
+		"object/mobile/dressed_criminal_thug_zabrak_male_01.iff"
+	},
+	lootGroups = {},
+	weapons = {},
+	conversationTemplate = "durgur_pyne_mission_target_convotemplate",
+	reactionStf = "@npc_reaction/slang",
+	attacks = merge(brawlermaster, teraskasinovice)
+}
+
+CreatureTemplates:addCreatureTemplate(pirate_quest_durgur, "pirate_quest_durgur")

MMOCoreORB/bin/scripts/screenplays/tasks/talus/durgur_pyne.lua
@@ -1,0 +1,89 @@
+durgur_pyne_missions =
+	{
+		{
+			missionType = "escort",
+			primarySpawns =
+			{
+				{ npcTemplate = "pirate_quest_durgur", npcName = "a Sleemo Lieutenant" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "sleemo_scamp", npcName = "" },
+				{ npcTemplate = "sleemo_scamp", npcName = "" }
+			},
+			itemSpawns = {},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 25 },
+			}
+		},
+		{
+			missionType = "escort",
+			primarySpawns =
+			{
+				{ npcTemplate = "agent_quest_durgur", npcName = "" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "sleemo_vandal", npcName = "" },
+				{ npcTemplate = "sleemo_scamp", npcName = "" },
+				{ npcTemplate = "sleemo_scamp", npcName = "" }
+			},
+			itemSpawns =
+			{
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 50 },
+			}
+		},
+		{
+			missionType = "assassinate",
+			primarySpawns =
+			{
+				{ npcTemplate = "pirate_captain", npcName = "a Pirate Captain" }
+			},
+			secondarySpawns =
+			{
+				{ npcTemplate = "sleemo_delinquent", npcName = "" },
+				{ npcTemplate = "sleemo_hoodlum", npcName = "" },
+				{ npcTemplate = "sleemo_hoodlum", npcName = "" },
+				{ npcTemplate = "sleemo_hoodlum", npcName = "" }
+			},
+			itemSpawns =
+			{
+			},
+			rewards =
+			{
+				{ rewardType = "credits", amount = 75 },
+			}
+		}
+	}
+
+npcMapDurgurPyne =
+	{
+		{
+			spawnData = { npcTemplate = "durgur_pyne", x = 3.0, z = -0.1, y = 4.4, direction = -22, cellID = 4265614, position = STAND },
+			worldPosition = { x = 4546, y = 5149 },
+			npcNumber = 1,
+			stfFile = "@static_npc/talus/talus_nashal_durgur_pyne",
+			missions = durgur_pyne_missions
+		},
+	}
+
+DurgurPyne = ThemeParkLogic:new {
+	npcMap = npcMapDurgurPyne,
+	className = "DurgurPyne",
+	screenPlayState = "durgur_pyne_tasks",
+	planetName = "talus",
+	distance = 900
+}
+
+registerScreenPlay("DurgurPyne", true)
+
+durgur_pyne_mission_giver_conv_handler = mission_giver_conv_handler:new {
+	themePark = DurgurPyne
+}
+durgur_pyne_mission_target_conv_handler = mission_target_conv_handler:new {
+	themePark = DurgurPyne
+}