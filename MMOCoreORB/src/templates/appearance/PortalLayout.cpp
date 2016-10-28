swgemu/Core3

[fixed] NGE houses failing to load
Change-Id: I538357034fd710fb950f197e2e3cc0f22b7c4bc7

MMOCoreORB/src/templates/appearance/PortalLayout.cpp
@@ -11,7 +11,7 @@
 #include "templates/appearance/FloorMesh.h"
 #include "engine/util/u3d/AStarAlgorithm.h"
 
-void PortalLayout::readPortalGeometry(IffStream *iff, int numPortals) {
+void PortalLayout::readPortalGeometry_0003(IffStream *iff, int numPortals) {
 
 	for(int i=0; i<numPortals; i++) {
 		iff->openChunk('PRTL');
@@ -60,7 +60,7 @@ void PortalLayout::readPortalGeometry(IffStream *iff, int numPortals) {
 			Vector3 &vert = verts->get(i);
 
 			vert = center + ((vert - center) * 1.1);
-			
+
 			// Triangle fan
 			if ( i >= 2) {
 				MeshTriangle triA;
@@ -80,6 +80,73 @@ void PortalLayout::readPortalGeometry(IffStream *iff, int numPortals) {
 	}
 }
 
+
+void PortalLayout::readPortalGeometry_0004(IffStream *iff, int numPortals) {
+
+	for(int i=0; i<numPortals; i++) {
+		iff->openForm('IDTL');
+		iff->openForm('0000');
+		Chunk *vertChunk = iff->openChunk('VERT');
+		uint32 size = vertChunk->getChunkSize() / 12;
+
+		Reference<PortalGeometry*> portal = new PortalGeometry();
+		Reference<MeshData*> mesh = portal->getGeometry();
+		Vector<Vector3> *verts = mesh->getVerts();
+		Vector<MeshTriangle> *tris = mesh->getTriangles();
+
+		Vector3 min(50000, 50000, 50000);
+		Vector3 max(-50000, -50000, -50000);
+
+		for (int i=0; i<size; i++) {
+			float x = iff->getFloat();
+			float y = iff->getFloat();
+			float z = iff->getFloat();
+
+			if(x < min.getX())
+				min.setX(x);
+
+			if(x > max.getX())
+				max.setX(x);
+
+			if(y < min.getY())
+				min.setY(y);
+
+			if (y > max.getY())
+				max.setY(y);
+
+			if (z < min.getZ())
+				min.setZ(z);
+
+			if (z > max.getZ())
+				max.setZ(z);
+
+			Vector3 vert(x, y, z);
+			verts->add(vert);
+		}
+
+		iff->closeChunk('VERT');
+
+		portal->setBoundingBox(AABB(min, max));
+		Vector3 center = portal->getBoundingBox().center();
+
+		Chunk *indxChunk = iff->openChunk('INDX');
+
+		uint32 numIdx = vertexDataChunk->getChunkSize() / 12;
+
+		for (int i=0; i<numIdx; i++) {
+			int a = iff->getInt();
+			int b = iff->getInt();
+			int c = iff->getInt();
+			tris->add(MeshTriangle(c, b, a));
+		}
+
+		iff->closeChunk('INDX');
+		iff->closeForm('0000');
+		iff->closeForm('IDTL');
+
+		portalGeometry.add(portal);
+	}
+}
 PortalLayout::PortalLayout() {
 	pathGraph = NULL;
 
@@ -115,7 +182,12 @@ void PortalLayout::parse(IffStream* iffStream) {
 		iffStream->closeChunk('DATA');
 
 		iffStream->openForm('PRTS');
-		readPortalGeometry(iffStream, numPortals);
+
+		if (type == '0003')
+			readPortalGeometry_0003(iffStream, numPortals);
+		else
+			readPortalGeometry_0004(iffStream, numPortals);
+
 		iffStream->closeForm('PRTS');
 
 		//open CELS form

MMOCoreORB/src/templates/appearance/PortalLayout.h
@@ -48,7 +48,8 @@ public:
 		return pathGraph;
 	}
 
-	void readPortalGeometry(IffStream *iff, int numPortals);
+	void readPortalGeometry_0003(IffStream *iff, int numPortals);
+	void readPortalGeometry_0004(IffStream *iff, int numPortals);
 
 	void readObject(IffStream* templateData) {
 		parse(templateData);

TheTinyPebble/swgtcw-test

MMOCoreORB/src/templates/appearance/PortalLayout.cpp
@@ -11,7 +11,7 @@
 #include "templates/appearance/FloorMesh.h"
 #include "engine/util/u3d/AStarAlgorithm.h"
 
-void PortalLayout::readPortalGeometry(IffStream *iff, int numPortals) {
+void PortalLayout::readPortalGeometry0003(IffStream *iff, int numPortals) {
 
 	for(int i=0; i<numPortals; i++) {
 		iff->openChunk('PRTL');
@@ -60,7 +60,7 @@ void PortalLayout::readPortalGeometry(IffStream *iff, int numPortals) {
 			Vector3 &vert = verts->get(i);
 
 			vert = center + ((vert - center) * 1.1);
-			
+
 			// Triangle fan
 			if ( i >= 2) {
 				MeshTriangle triA;
@@ -80,6 +80,73 @@ void PortalLayout::readPortalGeometry(IffStream *iff, int numPortals) {
 	}
 }
 
+
+void PortalLayout::readPortalGeometry0004(IffStream *iff, int numPortals) {
+
+	for(int i=0; i<numPortals; i++) {
+		iff->openForm('IDTL');
+		iff->openForm('0000');
+		Chunk *vertChunk = iff->openChunk('VERT');
+		uint32 size = vertChunk->getChunkSize() / 12;
+
+		Reference<PortalGeometry*> portal = new PortalGeometry();
+		Reference<MeshData*> mesh = portal->getGeometry();
+		Vector<Vector3> *verts = mesh->getVerts();
+		Vector<MeshTriangle> *tris = mesh->getTriangles();
+
+		Vector3 min(50000, 50000, 50000);
+		Vector3 max(-50000, -50000, -50000);
+
+		for (int i=0; i<size; i++) {
+			float x = iff->getFloat();
+			float y = iff->getFloat();
+			float z = iff->getFloat();
+
+			if(x < min.getX())
+				min.setX(x);
+
+			if(x > max.getX())
+				max.setX(x);
+
+			if(y < min.getY())
+				min.setY(y);
+
+			if (y > max.getY())
+				max.setY(y);
+
+			if (z < min.getZ())
+				min.setZ(z);
+
+			if (z > max.getZ())
+				max.setZ(z);
+
+			Vector3 vert(x, y, z);
+			verts->add(vert);
+		}
+
+		iff->closeChunk('VERT');
+
+		portal->setBoundingBox(AABB(min, max));
+		Vector3 center = portal->getBoundingBox().center();
+
+		Chunk *indxChunk = iff->openChunk('INDX');
+
+		uint32 numIdx = indxChunk->getChunkSize() / 12;
+
+		for (int i=0; i<numIdx; i++) {
+			int a = iff->getInt();
+			int b = iff->getInt();
+			int c = iff->getInt();
+			tris->add(MeshTriangle(c, b, a));
+		}
+
+		iff->closeChunk('INDX');
+		iff->closeForm('0000');
+		iff->closeForm('IDTL');
+
+		portalGeometry.add(portal);
+	}
+}
 PortalLayout::PortalLayout() {
 	pathGraph = NULL;
 
@@ -115,7 +182,12 @@ void PortalLayout::parse(IffStream* iffStream) {
 		iffStream->closeChunk('DATA');
 
 		iffStream->openForm('PRTS');
-		readPortalGeometry(iffStream, numPortals);
+
+		if (type == '0003')
+			readPortalGeometry0003(iffStream, numPortals);
+		else
+			readPortalGeometry0004(iffStream, numPortals);
+
 		iffStream->closeForm('PRTS');
 
 		//open CELS form

MMOCoreORB/src/templates/appearance/PortalLayout.h
@@ -48,7 +48,8 @@ public:
 		return pathGraph;
 	}
 
-	void readPortalGeometry(IffStream *iff, int numPortals);
+	void readPortalGeometry0003(IffStream *iff, int numPortals);
+	void readPortalGeometry0004(IffStream *iff, int numPortals);
 
 	void readObject(IffStream* templateData) {
 		parse(templateData);