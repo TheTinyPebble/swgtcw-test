swgemu/Core3

[added] Structure and collidable COV receiver types [refactored] Most existing safeCopyTo calls to use specific receivers
Change-Id: I088ea62e0e40451ece4087449e474a235c5659d4

MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -2116,7 +2116,7 @@ void CombatManager::broadcastCombatSpam(TangibleObject* attacker, TangibleObject
 
 	if (vec != NULL) {
 		closeObjects.removeAll(vec->size(), 10);
-		vec->safeCopyTo(closeObjects);
+		vec->safeCopyReceiversTo(closeObjects, CloseObjectsVector::CREOTYPE);
 	} else {
 #ifdef COV_DEBUG
 		info("Null closeobjects vector in CombatManager::broadcastCombatSpam", true);
@@ -2525,7 +2525,7 @@ Reference<SortedVector<ManagedReference<TangibleObject*> >* > CombatManager::get
 
 		if (vec != NULL) {
 			closeObjects.removeAll(vec->size(), 10);
-			vec->safeCopyTo(closeObjects);
+			vec->safeCopyReceiversTo(closeObjects, CloseObjectsVector::CREOTYPE);
 		} else {
 #ifdef COV_DEBUG
 			attacker->info("Null closeobjects vector in CombatManager::getAreaTargets", true);

MMOCoreORB/src/server/zone/managers/gcw/GCWManagerImplementation.cpp
@@ -1591,7 +1591,7 @@ void GCWManagerImplementation::broadcastBuilding(BuildingObject* building, Strin
 		zone->getInRangeObjects(building->getPositionX(), building->getPositionY(), range, &closeObjects, true);
 	} else {
 		CloseObjectsVector* closeVector = (CloseObjectsVector*) building->getCloseObjects();
-		closeVector->safeCopyTo(closeObjects);
+		closeVector->safeCopyReceiversTo(closeObjects, CloseObjectsVector::CREOTYPE);
 	}
 
 	// send message to all the players in range

MMOCoreORB/src/server/zone/objects/creature/commands/BroadcastAreaCommand.h
@@ -76,7 +76,7 @@ public:
 		}
 		else {
 			CloseObjectsVector* closeVector = (CloseObjectsVector*) creature->getCloseObjects();
-			closeVector->safeCopyTo(closeObjects);
+			closeVector->safeCopyReceiversTo(closeObjects, CloseObjectsVector::CREOTYPE);
 		}
 
 		//Command Options

MMOCoreORB/src/server/zone/objects/creature/events/DroidMerchantBarkerTask.h
@@ -84,7 +84,7 @@ public:
 		CloseObjectsVector* vec = (CloseObjectsVector*) droid->getCloseObjects();
 		SortedVector<QuadTreeEntry*> closeEntryObjects(200, 50);
 		if (vec != NULL) {
-			vec->safeCopyTo(closeEntryObjects);
+			vec->safeCopyReceiversTo(closeEntryObjects, CloseObjectsVector::CREOTYPE);
 		} else {
 #ifdef COV_DEBUG
 			droid->info("Null closeobjects vector in DroidMerchantBarkerTask::run()", true);

MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
@@ -2205,7 +2205,7 @@ void PlayerObjectImplementation::updateInRangeBuildingPermissions() {
 	CloseObjectsVector* vec = (CloseObjectsVector*) parent->getCloseObjects();
 
 	SortedVector<QuadTreeEntry*> closeObjects;
-	vec->safeCopyTo(closeObjects);
+	vec->safeCopyReceiversTo(closeObjects, StructureObject::STRUCTURECOVTYPE);
 
 	for (int i = 0; i < closeObjects.size(); ++i) {
 		BuildingObject* building = cast<BuildingObject*>(closeObjects.get(i));

TheTinyPebble/swgtcw-test

MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -2116,7 +2116,7 @@ void CombatManager::broadcastCombatSpam(TangibleObject* attacker, TangibleObject
 
 	if (vec != NULL) {
 		closeObjects.removeAll(vec->size(), 10);
-		vec->safeCopyTo(closeObjects);
+		vec->safeCopyReceiversTo(closeObjects, CloseObjectsVector::PLAYERTYPE);
 	} else {
 #ifdef COV_DEBUG
 		info("Null closeobjects vector in CombatManager::broadcastCombatSpam", true);

MMOCoreORB/src/server/zone/managers/gcw/GCWManagerImplementation.cpp
@@ -1591,7 +1591,7 @@ void GCWManagerImplementation::broadcastBuilding(BuildingObject* building, Strin
 		zone->getInRangeObjects(building->getPositionX(), building->getPositionY(), range, &closeObjects, true);
 	} else {
 		CloseObjectsVector* closeVector = (CloseObjectsVector*) building->getCloseObjects();
-		closeVector->safeCopyTo(closeObjects);
+		closeVector->safeCopyReceiversTo(closeObjects, CloseObjectsVector::PLAYERTYPE);
 	}
 
 	// send message to all the players in range

MMOCoreORB/src/server/zone/objects/creature/commands/BroadcastAreaCommand.h
@@ -76,7 +76,7 @@ public:
 		}
 		else {
 			CloseObjectsVector* closeVector = (CloseObjectsVector*) creature->getCloseObjects();
-			closeVector->safeCopyTo(closeObjects);
+			closeVector->safeCopyReceiversTo(closeObjects, CloseObjectsVector::PLAYERTYPE);
 		}
 
 		//Command Options

MMOCoreORB/src/server/zone/objects/creature/events/DroidMerchantBarkerTask.h
@@ -84,7 +84,7 @@ public:
 		CloseObjectsVector* vec = (CloseObjectsVector*) droid->getCloseObjects();
 		SortedVector<QuadTreeEntry*> closeEntryObjects(200, 50);
 		if (vec != NULL) {
-			vec->safeCopyTo(closeEntryObjects);
+			vec->safeCopyReceiversTo(closeEntryObjects, CloseObjectsVector::PLAYERTYPE);
 		} else {
 #ifdef COV_DEBUG
 			droid->info("Null closeobjects vector in DroidMerchantBarkerTask::run()", true);

MMOCoreORB/src/server/zone/objects/player/PlayerObjectImplementation.cpp
@@ -2205,7 +2205,7 @@ void PlayerObjectImplementation::updateInRangeBuildingPermissions() {
 	CloseObjectsVector* vec = (CloseObjectsVector*) parent->getCloseObjects();
 
 	SortedVector<QuadTreeEntry*> closeObjects;
-	vec->safeCopyTo(closeObjects);
+	vec->safeCopyReceiversTo(closeObjects, CloseObjectsVector::STRUCTURETYPE);
 
 	for (int i = 0; i < closeObjects.size(); ++i) {
 		BuildingObject* building = cast<BuildingObject*>(closeObjects.get(i));