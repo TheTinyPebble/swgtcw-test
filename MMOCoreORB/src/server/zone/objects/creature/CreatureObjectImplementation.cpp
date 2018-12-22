70b328e77abcf8a4faa6aedef1a71ada03fc0c99
c0pp3r <dominguez.joey@gmail.com>
2018-12-22 00:34:32 -0500

    [fixed] Syntax is LS TEF

--- MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/CreatureObjectImplementation.cpp
@@ -3050,7 +3050,7 @@ bool CreatureObjectImplementation::isAttackableBy(CreatureObject* object, bool b
 		return true;
 	}
 
-	if ((ghost->hasJediTef() && ((object->getFaction() != getFaction()) && (object->getFaction() !=0)))  || ((ghost->isJedi() && getWeapon()->isJediWeapon()) && ((object->getFaction() != getFaction()) && (object->getFaction() !=0))) || ((ghost->isJediAttackable() && ((object->getFaction()) != getFaction()) && (object->getFaction() !=0)))){
+	if ((ghost->hasJediTef() && ((object->getFaction() != getFaction()) || (object->getFaction() !=0)))  || ((ghost->isJedi() && getWeapon()->isJediWeapon()) && ((object->getFaction() != getFaction()) || (object->getFaction() !=0))) || ((ghost->isJediAttackable() && ((object->getFaction()) != getFaction()) || (object->getFaction() !=0)))){
 		return true;
 	}
 
