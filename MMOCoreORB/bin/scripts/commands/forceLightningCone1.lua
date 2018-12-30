600008d6b8b6644c0fab1b96e03dac604488b752
Alpha <jamesmorris9@hotmail.com>
2018-12-30 17:28:22 +0000

    [fixed] Changed Lightning to LS damage

--- MMOCoreORB/bin/scripts/commands/forceLightningCone1.lua
+++ MMOCoreORB/bin/scripts/commands/forceLightningCone1.lua
@@ -61,7 +61,7 @@ ForceLightningCone1Command = {
 	poolsToDamage = RANDOM_ATTRIBUTE,
 
 	forceAttack = true,
-	damageType = ELECTRICITY_DAMAGE,
+	damageType = LIGHTSABER_DAMAGE,
 
 	frsLightMinDamageModifier = 4,
 	frsLightMaxDamageModifier = 4,
