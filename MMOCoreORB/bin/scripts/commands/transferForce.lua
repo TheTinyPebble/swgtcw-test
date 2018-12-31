0f8aa0b7a9730b0ed699a9b1642acd5612b81113
Alpha <jamesmorris9@hotmail.com>
2018-12-31 00:15:21 +0000

    [fixed] Fixed drain and transfer

--- MMOCoreORB/bin/scripts/commands/transferForce.lua
+++ MMOCoreORB/bin/scripts/commands/transferForce.lua
@@ -43,15 +43,14 @@
 
 TransferForceCommand = {
 	name = "transferforce",
-	
+
 	animation = "force_transfer_1",
 
 	combatSpam = "forcetransfer_hit",
-	
-	minDamage = 200, --Force transferred
-	
+
+	minDamage = 500, --Force transferred
+
 	range = 32
 }
 
 AddCommand(TransferForceCommand)
-
