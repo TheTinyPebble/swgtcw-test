0f4a399c74d1778708b1eb93f86cb14a2a57ae0a
Alpha <jamesmorris9@hotmail.com>
2018-12-31 22:13:40 +0000

    [fixed] Throw dot pvp reduction v3

--- MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
+++ MMOCoreORB/src/server/zone/managers/combat/CombatManager.cpp
@@ -561,7 +561,7 @@ void CombatManager::applyDots(CreatureObject* attacker, CreatureObject* defender
 		if (potency == 0) {
 			potency = 150;
 		}
-		if (potency > 7000 && defender->isPlayerCreature()) {
+		if (dotStrength > 999 && defender->isPlayerCreature()) {
 			potency = 300;
 			dotstrength = 100;
 		}
