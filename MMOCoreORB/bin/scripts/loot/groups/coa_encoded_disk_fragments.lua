f024fc7796319800691fb9ccd6816ceacf253942
Alpha <jamesmorris9@hotmail.com>
2019-01-27 20:51:40 +0000

    [Removed] COA Disks

--- MMOCoreORB/bin/scripts/loot/groups/coa_encoded_disk_fragments.lua
+++ MMOCoreORB/bin/scripts/loot/groups/coa_encoded_disk_fragments.lua
@@ -3,14 +3,7 @@ coa_encoded_disk_fragments = {
 	minimumLevel = 0,
 	maximumLevel = 0,
 	lootItems = {
-		{itemTemplate = "coa_imp_1", weight = 1250000},
-		{itemTemplate = "coa_imp_2", weight = 1250000},
-		{itemTemplate = "coa_imp_3", weight = 1250000},
-		{itemTemplate = "coa_imp_4", weight = 1250000},
-		{itemTemplate = "coa_reb_1", weight = 1250000},
-		{itemTemplate = "coa_reb_2", weight = 1250000},
-		{itemTemplate = "coa_reb_3", weight = 1250000},
-		{itemTemplate = "coa_reb_4", weight = 1250000},
+		{itemTemplate = "force_color_crystal", weight = 10000000}
 	}
 }
 
