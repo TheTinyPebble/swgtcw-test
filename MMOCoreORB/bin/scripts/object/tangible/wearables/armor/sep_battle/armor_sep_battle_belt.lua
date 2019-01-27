1e939e3ec45b1c7d07ea9c43e2ec717681768c85
Alpha <jamesmorris9@hotmail.com>
2019-01-27 18:00:10 +0000

    [Updated] Medium Armour Encumb

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/sep_battle/armor_sep_battle_belt.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/sep_battle/armor_sep_battle_belt.lua
@@ -126,15 +126,15 @@ object_tangible_wearables_armor_rebel_battle_armor_rebel_battle_belt = object_ta
 	acid = 70,
 	lightsaber = 0,
 
-	numberExperimentalProperties = {1, 1, 1, 1, 2, 2, 2, 2, 2, 1, 1, 2, 1},
-	experimentalProperties = {"XX", "XX", "XX", "XX", "OQ", "SR", "OQ", "UT", "MA", "OQ", "MA", "OQ", "MA", "OQ", "XX", "XX", "OQ", "SR", "XX"},
-	experimentalWeights = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	experimentalGroupTitles = {"null", "null", "null", "exp_durability", "exp_quality", "exp_resistance", "exp_durability", "exp_durability", "exp_durability", "null", "null", "exp_resistance", "null"},
-	experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
-	experimentalMin = {0, 0, 0, 1000, 1, 30000, 250, 75, 25, 1, 256, 0, 0},
-	experimentalMax = {0, 0, 0, 1000, 20, 50000, 150, 45, 15, 1, 256, 0, 0},
-	experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
-	experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
+	numberExperimentalProperties = {1, 1, 1},
+	experimentalProperties = {"XX", "XX", "XX"},
+	experimentalWeights = {1, 1, 1},
+	experimentalGroupTitles = {"null", "null", "null"},
+	experimentalSubGroupTitles = {"null", "null", "sockets"},
+	experimentalMin = {0, 0, 0},
+	experimentalMax = {0, 0, 0},
+	experimentalPrecision = {0, 0, 0},
+	experimentalCombineType = {0, 0, 4},
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_armor_rebel_battle_armor_rebel_battle_belt, "object/tangible/wearables/armor/rebel_battle/armor_rebel_battle_belt.iff")
