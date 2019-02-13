4b0c5247a3b9b8191f1c8a962608171afd8bc489
Tiny Pebble <npoulsen@uclan.ac.uk>
2019-02-13 22:57:57 +0000

    [Added] Splitting vendor tokens functionality
    Change-Id: I7a1a8c5cd220e2ba7b10852a5b367ec9b0efba3e

---
+++ MMOCoreORB/src/server/zone/objects/player/sui/callbacks/SplitTokenSuiCallback.h
@@ -0,0 +1,58 @@
+#ifndef SPLIT_TOKEN_SUI_CALLBACK
+#define SPLIT_TOKEN_SUI_CALLBACK
+
+#include "server/zone/objects/player/sui/SuiCallback.h"
+#include "server/zone/objects/player/sui/callbacks/SplitTokenSuiCallback.h"
+#include "server/zone/objects/player/sui/inputbox/SuiInputBox.h"
+#include "server/zone/objects/tangible/misc/VendorToken.h"
+
+class SplitTokenSuiCallback : public SuiCallback {
+	ManagedWeakReference<SceneObject*> scenObj;
+	
+public:
+	SplitTokenSuiCallback(ZoneServer* server, SceneObject* sceneObject) : SuiCallback(server) {
+		scenObj = sceneObject;
+	}
+
+	void run(CreatureObject* player, SuiBox* suiBox, uint32 eventIndex, Vector<UnicodeString>* args) {
+		if (!player->isPlayerCreature()) {
+		return;
+		}
+		
+		SceneObject* inventory = player->getSlottedObject("inventory");
+		if (inventory == NULL) {
+			return;
+		}
+		
+		bool cancelPressed = (eventIndex == 1);
+
+		if (!suiBox->isTransferBox() || cancelPressed || args->size() <= 1) {
+			return;
+		}
+
+		int value = Integer::valueOf(args->get(1).toString());
+		SceneObject* sceno = scenObj.get();
+		
+		String fullTemplate = sceno->getObjectTemplate()->getFullTemplateString();
+		TangibleObject* tano = cast <TangibleObject* >(sceno);
+		ManagedReference<TangibleObject*> newToken = player->getZoneServer()->createObject(fullTemplate.hashCode(), 1).castTo<TangibleObject*>();
+		
+		if (newToken == NULL) {
+			return;
+		}
+		
+		Locker locker(tano);
+		tano->setUseCount(tano->getUseCount() - value, true);
+		
+		Locker newTokenLocker(newToken);
+		
+		inventory->transferObject(newToken, -1, true);
+		
+		newToken->sendTo(player, true);
+		newToken->setUseCount(value, true);
+
+		player->sendSystemMessage("You've successfully split your items.");
+	}
+};
+
+#endif
