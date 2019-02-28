38063ad6a404f8a06d5929b36790211a8673da70
Dirtnose <dirtnose@gmail.com>
2019-02-28 21:51:38 +0000

    Add crafting info to knight robes

--- MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_light_s01.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_light_s01.lua
@@ -67,6 +67,16 @@ object_tangible_wearables_robe_robe_jedi_light_s01 = object_tangible_wearables_r
 
                 certificationsRequired = {"force_title_jedi_rank_01"},
 				attributeListComponent = "JediRobeAttributeListComponent",
+
+				numberExperimentalProperties = {1, 1, 1, 1},
+				experimentalProperties = {"XX", "XX", "XX", "XX"},
+				experimentalWeights = {1, 1, 1, 1},
+				experimentalGroupTitles = {"null", "null", "null", "null"},
+				experimentalSubGroupTitles = {"null", "null", "sockets", "hitpoints"},
+				experimentalMin = {0, 0, 0, 1000},
+				experimentalMax = {0, 0, 0, 1000},
+				experimentalPrecision = {0, 0, 0, 0},
+				experimentalCombineType = {0, 0, 4, 4},
 }
 
 ObjectTemplates:addTemplate(object_tangible_wearables_robe_robe_jedi_light_s01, "object/tangible/wearables/robe/robe_jedi_light_s01.iff")
