f6d8f4409cb3ef7eee92bee0f9d7c9a2683caabc
Dirtnose <dirtnose@gmail.com>
2019-03-20 16:22:16 +0000

    Turret tuning to compensate for rangedmit2 on Jedi and AP on non jedi.

--- MMOCoreORB/bin/scripts/object/weapon/ranged/turret/turret_block_large.lua
+++ MMOCoreORB/bin/scripts/object/weapon/ranged/turret/turret_block_large.lua
@@ -47,7 +47,7 @@ object_weapon_ranged_turret_turret_block_large = object_weapon_ranged_turret_sha
 	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
 	damageType = BLAST,
 	-- NONE, LIGHT, MEDIUM, HEAVY
-	armorPiercing = HEAVY,
+	armorPiercing = LIGHT,
 	-- See http://www.ocdsoft.com/files/accuracy.xls
 	creatureAccuracyModifiers = { "rifle_accuracy" },
 	creatureAimModifiers = { "rifle_aim", "aim" },
@@ -66,8 +66,8 @@ object_weapon_ranged_turret_turret_block_large = object_weapon_ranged_turret_sha
 	idealAccuracy = 70,
 	maxRange = 80,
 	maxRangeAccuracy = 0,
-	minDamage = 750,
-	maxDamage = 1200,
+	minDamage = 800,
+	maxDamage = 900,
 	attackSpeed = 1,
 	woundsRatio = 9,
 }
