491cb3bedadb719f09ebf8754ccae7c3161a7f11
Dirtnose <dirtnose@gmail.com>
2019-03-23 13:21:19 +0000

    disable Rep PSG quest for now

--- MMOCoreORB/bin/scripts/screenplays/tasks/dantooine/xaan_talmaron.lua
+++ MMOCoreORB/bin/scripts/screenplays/tasks/dantooine/xaan_talmaron.lua
@@ -79,7 +79,7 @@ XaanTalmaron = ThemeParkLogic:new {
 	faction = FACTIONIMPERIAL
 }
 
-registerScreenPlay("XaanTalmaron", true)
+registerScreenPlay("XaanTalmaron", false)
 
 xaan_talmaron_mission_giver_conv_handler = mission_giver_conv_handler:new {
 	themePark = XaanTalmaron
