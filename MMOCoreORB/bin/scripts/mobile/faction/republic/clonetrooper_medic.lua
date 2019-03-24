68bf9198f2085ec6b00d08fd03342a9a51f36d5b
Alpha <jamesmorris9@hotmail.com>
2019-03-24 18:53:13 +0000

    [fixed] Normal clones no longer spawn with 501st armour

--- MMOCoreORB/bin/scripts/mobile/faction/republic/clonetrooper_medic.lua
+++ MMOCoreORB/bin/scripts/mobile/faction/republic/clonetrooper_medic.lua
@@ -28,10 +28,7 @@ clonetrooper_medic = Creature:new {
 	diet = HERBIVORE,
 	scale = 1.05,
 
-	templates = {"object/mobile/dressed_clonetrooper_m.iff",
-		     "object/mobile/dressed_clonetrooper_blue_m.iff",
-		     "object/mobile/dressed_clonetrooper_blue_nh_m.iff"
-	},
+	templates = {"object/mobile/dressed_clonetrooper_m.iff",	},
 	lootGroups = {
 		{
 			groups = {
