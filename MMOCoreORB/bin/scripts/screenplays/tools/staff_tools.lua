a77dae8c93be956945f6d1818b5c571599f4707e
c0pp3r <dominguez.joey@gmail.com>
2019-04-25 08:05:13 -0400

    [Fixed] Try creature object

--- MMOCoreORB/bin/scripts/screenplays/tools/staff_tools.lua
+++ MMOCoreORB/bin/scripts/screenplays/tools/staff_tools.lua
@@ -263,7 +263,7 @@ function StaffTools.resetHolocronCooldown(pPlayer)
 		return
 	end
 
-	writeScreenPlayData(pGhost, "CustomJediProgression", "HolocronTimer", 999)
+	writeScreenPlayData(pTarget, "CustomJediProgression", "HolocronTimer", 999)
 	CreatureObject(pPlayer):sendSystemMessage("Now force resetting holocron cooldown for " .. playerName .. ".")
 
 end
