7b3159b7510f15e7e338c59eceba686ba9b90484
Vikoslak <vikoslak18@yahoo.com>
2019-05-19 09:41:38 -0500

    Junk Loot Fix Attempt

--- MMOCoreORB/bin/scripts/loot/items/junk/satchel.lua
+++ MMOCoreORB/bin/scripts/loot/items/junk/satchel.lua
@@ -7,7 +7,7 @@ satchel = {
 	},
 	customizationStringNames = {},
 	customizationValues = {},
-	junkDealerTypeNeeded = JUNKGENERIC,
+	junkDealerTypeNeeded = {JUNKGENERIC, JUNKFINERY, JUNKARMS},
 	junkMinValue = 20,
 	junkMaxValue = 40
 }
